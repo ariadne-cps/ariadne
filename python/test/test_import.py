321de9a9b79b399f013cdb4666c9b182e583a3a6
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2006-09-21 07:17:54 +0000

    Added test_import.py to the Python test suite to check if all modules can be imported. Removed all other tests as these are out-of-date.

--- python/test/test_import.py
+++ python/test/test_import.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 ##############################################################################
-#            test_polyhedron.py
+#            test_import.py
 #
 #  Copyright 2006  Pieter Collins <Pieter.Collins@cwi.nl>
 ##############################################################################
@@ -20,52 +20,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
-
 from ariadne.base import *
-from ariadne.geometry import *
+from ariadne.numeric import *
 from ariadne.linear_algebra import *
+from ariadne.combinatoric import *
+from ariadne.geometry import *
 from ariadne.system import *
+from ariadne.evaluation import *
 from ariadne.output import *
-
-sl=PointList()
-s=Point(2)
-s[0]=0.125
-s[1]=0.125
-sl.append(s)
-s[0]=2.375
-sl.append(s)
-s[1]=2.375
-sl.append(s)
-r=Simplex(sl)
-s[0]=1.5
-sl.append(s)
-q=Polyhedron(sl)
-
-A=Matrix(2,2);
-b=Vector(2);
-
-A[0,0]=-1.0;
-A[1,1]=-1.0;
-
-M=AffineMap(A,b)
-
-q2=M(q);
-
-c=convex_hull(q,q2);
-
-eps=EpsPlot("polyhedron.eps",Rectangle("[-3,3]x[-3,3]"))
-eps.set_fill_colour("yellow")
-
-eps.write(c)
-
-eps.set_fill_colour("blue")
-
-eps.write(q)
-eps.write(q2)
-
-eps.close()
-
-print sl
-print r
-print q
-print q.vertices()

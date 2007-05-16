29c875903c94277eb487383284e4b11f968ee587
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2007-05-16 10:42:42 +0000

    Added documentation for Differential classes.

--- include/numeric/differential.h
+++ include/numeric/differential.h
@@ -43,9 +43,33 @@
 namespace Ariadne {
   namespace Numeric {
 
-
     /*!\ingroup Numeric
-     * \brief A templated class representing a the differential of a quantity.
+     * \brief A templated class representing a quantity and its first variation or differential with respect to one or more independent variables.
+     *
+     * A differential \f$(x,v)\f$ or \f$(x,\dot{x})\f$ represents a quantity whose value is \f$x\f$ and whose derivative with respect to the ith independent
+     * variable is \f$\partial x/\partial t_i=v_i\f$. A
+     * A differential of the form \f$(c,0)\f$ represents a constant. 
+     * A differential of the form \f$(x,e_i)\f$, where \f$e_i\f$ is the ith unit vector, represents the independent variable \f$x_i\f$.
+     *
+     * Addition of differentials is given by \f[(x,v)+(y,w):=(x+y,v+w) . \f]
+     * By the Leibnitz rule, \f[(x,v)\times(y,w):=(xy,\;yv+xw) . \f]
+     * The action of a scalar function \f$f\f$ on the differential \f$(x,v)\f$ is defined \f[f(x,v):=(f(x),\;f'(x)v) . \f]
+     * The chain rule is automatically obtained: \f[(g\circ f)(x,v)=\bigl(g(f(x)),\;g'(f(x))f'(x)v\bigr) . \f]
+     *
+     * The constructor Differential(x,v) constructs a variable whose value is \a x and whose partial derivative with respect to the
+     * \a i th independent variable is \a v[i] .
+     * To construct a "constant" quantity with respect to \a n arguments, use %Differential(c, zero_vector(n)) .
+     * To construct the \a i th "variable" x of \a n, use %Differential(x,unit_vector(n,i)) .
+     *
+     * The specialization Differential<X,X> represents a quantiy and its first variation with respect to a single independent variable.
+     * The SecondDifferential<X,X,X> class represents a quantity and its first two variations with respect to an independent variable.
+     *
+     * \internal 
+     * Maybe a better name for the class would be "Variation" or "Derivative". 
+     * The name "Differential" could then be used for backward differentiation. 
+     * Alternatively, "DifferentialForm" could be used for backward differentiation.
+     *
+     * This class needs to be specialised or extended to provide better support for vector-valued functions.
      */
     template<class X, class V>
     class Differential
@@ -59,13 +83,13 @@ namespace Ariadne {
       Differential() : _x(), _dx() { }
       /*! \brief Constuct a differential based on variable x and derivative vector dx. */
       template<class XX,class VV> Differential(const XX& x, const VV& dx) : _x(x), _dx(dx) { }
-      /*! \brief Construct the differential of a constant with respect to \a n independent variables. */
+      /*! \brief Construct the differential of a constant with respect to \a n independent variables. (Deprecated) */
       template<class XX> Differential(const XX& x, size_type n) : _x(x), _dx(n) { }
-      /*! \brief Construct the differential of the \a i th variable of \a n independent variables. */
+      /*! \brief Construct the differential of the \a i th variable of \a n independent variables. (Deprecated) */
       template<class XX> Differential(const XX& x, size_type n, size_type i) : _x(x), _dx(n) { _dx[i]=1; }
-      /*! \brief Assign the differential of a constant zero with respect to the current independent variables (useful as an alternative to a constructor). */
+      /*! \brief Assign the differential of a constant zero with respect to the current independent variables (useful as an alternative to a constructor).*/
       template<class XX> Differential<X,V> operator=(const XX& c) { 
-        this->_x=c; for(size_type j=0; j!=this->_dx.size(); ++j) { this->_dx[j]=0; } return *this; }
+        this->_x=c; this->_dx=this->_dx*0; return *this; }
       //@}
 
       //@{
@@ -74,15 +98,96 @@ namespace Ariadne {
       const X& value() const { return this->_x; }
       /*! \brief The differential of the variable with respect to the indepenent variables. */
       const V& derivative() const { return this->_dx; }
-      /*! \brief The differential of the variable with respect to the \a j th indepenent variable. */
+      /*! \brief The differential of the variable with respect to the \a j th indepenent variable. (Deprecated)  */
       const typename V::value_type& derivative(size_type j) const { return this->_dx[j]; }
       //@}
+      
+ #ifdef DOXYGEN
+      //@{
+      //! \name Comparison operators.
+      /*! \brief Equality operator. */
+      friend template<class X1, class V1, class X2, class V2> tribool operator==(const Differential<X1,V1>& x1, const Differential<X2,V2>& x2); 
+      /*! \brief Inequality operator. */
+      friend template<class X1, class V1, class X2, class V2> tribool operator!=(const Differential<X1,V1>& x1, const Differential<X2,V2>& x2); 
+      //@}
+     
+      //@{
+      //! \name Arithmetic operations
+      
+      /*! \brief The differential with the minimal value from \a x1 or \a x2. */
+      friend Differential<X,V> min(const Differential<X,V>& x1, const Differential<X,V>& x2);
+      /*! \brief The differential with the maximal value from \a x1 or \a x2. */
+      friend Differential<X,V> max(const Differential<X,V>& x1, const Differential<X,V>& x2);
+      /*! \brief Absolute value function. Returns \f$(-x,-\dot{x})\f$ if \f$x<0\f$ and \f$(x,\dot{x})\f$ if \f$x>0\f$. 
+       *  If interval arithmetic is used, returns \f$(0,[-1,1]\cdot\dot{x})\f$ at zero. */
+      friend Differential<X,V> abs(const Differential<X,V>& x);
+      
+      /*! \brief The positive value of \a x. Returns a copy \f$(x,\; \dot{x})\f$. */
+      friend Differential<X,V> operator+(const Differential<X,V>& x);
+      /*! \brief Negation of a differential. Returns \f$(-x,\; -\dot{x})\f$. */
+      friend Differential<X,V> operator-(const Differential<X,V>& x);
+      /*! \brief  Addition of differentials. Returns \f$(x_1+x_2,\; \dot{x}_1+\dot{x_2})\f$. Addition of a constant is also available. */
+      friend Differential<X,V> operator+(const Differential<X,V>& x1, const Differential<X,V>& x2);
+      /*! \brief  Subtraction of differentials. Returns \f$(x_1-x_2,\; \dot{x}_1-\dot{x_2})\f$. Subtraction of/from a constant is also available. */
+      friend Differential<X,V> operator-(const Differential<X,V>& x1, const Differential<X,V>& x2);
+      /*! \brief Multiplication of differentials. Returns \f$(x_1x_2,\; x_2\dot{x}_1+x_1\dot{x_2})\f$. Multiplication by a constant is also available. */
+      friend Differential<X,V> operator*(const Differential<X,V>& x1, const Differential<X,V>& x2);
+      /*! \brief Division of differentials. Returns \f$(x_1/x_2,\; \dot{x}_1/x2-x_1\dot{x_2}/x_2^2)\f$. Division of/by a constant is also available. */
+      friend Differential<X,V> operator/(const Differential<X,V>& x1, const Differential<X,V>& x2);
+      /*! \brief Integer power of a differential. Returns \f$(x^n,\; nx^{n-1}\dot{x})\f$. */
+      friend template<class N> Differential<X,V> pow(const Differential<X,V>& x, const N& n);
+      //@}
+      
+      //@{
+      //! \name Algebraic and transcendental operations
+      /*! \brief Square root. Returns \f$(\sqrt{x},\; \dot{x}/2\sqrt{x})\f$. */
+      friend Differential<X,V> sqrt(const Differential<X,V>& x); 
+      /*! \brief Exponential. Returns \f$(e^x,\; \dot{x}e^x)\f$. */
+      friend Differential<X,V> exp(const Differential<X,V>& x); 
+      /*! \brief Natural logarithm. Returns \f$(\log x,\; \dot{x}/x)\f$. */
+      friend Differential<X,V> log(const Differential<X,V>& x); 
+      /*! \brief Sine function. Returns \f$(\sin x,\; \dot{x}\cos x)\f$. */
+      friend Differential<X,V> sin(const Differential<X,V>& x); 
+      /*! \brief Cosine function. Returns \f$(\cos x,\; -\dot{x}\sin x)\f$. */
+      friend Differential<X,V> cos(const Differential<X,V>& x); 
+      /*! \brief Tangent function. Returns \f$(\tan x,\; \dot{x}\sec^2 x)\f$. */
+      friend Differential<X,V> tan(const Differential<X,V>& x); 
+      /*! \brief Inverse sine function. Returns \f$(\sin^{-1} x,\; \dot{x}/\sqrt{1-x^2})\f$. */
+      friend Differential<X,V> asin(const Differential<X,V>& x); 
+      /*! \brief Inverse cosine function. Returns \f$(\cos^{-1} x,\; \dot{x}/\sqrt{1-x^2})\f$. */
+      friend Differential<X,V> acos(const Differential<X,V>& x); 
+      /*! \brief Inverse tangent function. Returns \f$(\tan^{-1} x,\; \dot{x}/(1+x^2)\f$. */
+      friend Differential<X,V> atan(const Differential<X,V>& x); 
+      //@}
+      
+      //@{
+      //! \name Input/output operators.
+      /*! \brief Stream insertion operator. */
+      friend std::ostream& operator<<(std::ostream& os, const Differential<X,V>& x); 
+     //@}
+#endif
       
     };
     
     
     /*!\ingroup Numeric
-     * \brief A templated class representing a the differential of a quantity with respect to a scalar argument.
+     * \brief A templated class representing a the differential of a quantity with respect to a scalar argument, 
+     * which can be used to compute the derivative of a scalar-valued function.
+     *
+     * A scalar differential is a pair \f$(x,\dot{x})\f$ where \f$x\f$ is the value of the quantity,
+     * and \f$\dot{x}\f$ is the variation with respect to some independent variable. 
+     * A differential of the form \f$(c,0)\f$ represents a constant. 
+     * A differential of the form \f$(x,1)\f$ represents the independent variable with value \f$x\f$.
+     *
+     * The operation of a scalar function \f$f\f$ on the differential \f$(x,\dot{x})\f$ is defined \f[f(x,\dot{x}):=(f(x),f'(x)\dot{x}) . \f]
+     *
+     * To compute the derivative of a scalar function \f$f\f$ at a point \f$x\f$, evaluate f(%Differential<%Float,%Float>(x,1)).%derivative().
+     * To compute range of derivatives of a scalar function \f$f\f$ over an interval \f$I\f$, evaluate f(%Differential<%Interval,%Interval>(I,1)).%derivative().
+     *
+     * To construct a constant differential \a c, use Differential(c,0).
+     * To construct the differential of a variable \a x, use Differential(x,1).
+     * 
+     * See the documentation for the Differential template for a list of supported operations. See the SecondDifferential<X,X,X> template for computing second derivatives.
      */
     template<class X>
     class Differential< X, X >
@@ -96,6 +201,8 @@ namespace Ariadne {
       Differential() : _x(), _dx() { }
       /*! \brief Constuct a differential based on variable x and derivative dx. */
       template<class XX0,class XX1> Differential(const XX0& x, const XX1& dx) : _x(x), _dx(dx) { }
+      /*! \brief Assign a differential from a constant quantity. */
+      template<class XX> Differential<X,X>& operator=(const XX& c) { this->_x=c; this->_dx=0; return *this; }
       //@}
 
       //@{
@@ -330,7 +437,17 @@ namespace Ariadne {
     template<class X,class V, class H> class SecondDifferential;
 
     /*!\ingroup Numeric
-     * \brief A templated class representing a the second differential of a quantity with respect to a scalar argument.
+     * \brief A templated class representing a the second differential of a quantity with respect to a scalar argument,
+     * which can be used to compute the second derivative of a scalar-valued function.
+     *
+     * The image of the second differential \f$(x,\dot{x},\ddot{x})\f$ under a scalar function \f$f\f$ is 
+     *   \f[f(x,\dot{x},\ddot{x}):=(f(x),\;f'(x)\dot{x},\;f'(x)\ddot{x}+f''(x)\dot{x}^2) . \f]
+     *
+     * To compute the second derivative of a scalar function \f$f\f$ at a point \f$x\f$, evaluate f(%SecondDifferential<%Float,%Float,%FLoat>(x,1,0)).%second_derivative().
+     * To compute range of second of a scalar function \f$f\f$ over an interval \f$I\f$, evaluate f(%Differential<%Interval,%Interval,%Interval>(I,1,0)).%derivative().
+     *
+     * To construct a constant second differential \a c, use SecondDifferential(c,0,0).
+     * To construct the differential of a variable \a x, use SecondDifferential(x,1,0).
      */
     template<class X>
     class SecondDifferential< X, X, X >
@@ -345,6 +462,9 @@ namespace Ariadne {
       /*! \brief Constuct a second differential based on variable \a x, derivative \a dx and second derivative \a ddx. */
       template<class XX0,class XX1,class XX2> SecondDifferential(const XX0& x, const XX1& dx, const XX2& ddx)
         : _x(x), _dx(dx), _ddx(ddx) { }
+      /*! \brief Assign a second differential from a constant quantity. */
+      template<class XX> SecondDifferential<X,X,X>& operator=(const XX& c) { 
+        this->_x=c; this->_dx=0; this->_ddx=0; return *this; }
       //@}
 
       //@{

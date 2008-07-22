ec3c51a9d47560cfbe36924c5f9610991efbf1f0
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2008-07-22 16:05:39 +0000

    Added grazing detection for ImpactSystemEvolver.

--- test/test_impact_system_evolver.cc
+++ test/test_impact_system_evolver.cc
@@ -27,11 +27,11 @@
 #include "function/function_interface.h"
 #include "function/affine_function.h"
 #include "geometry/zonotope.h"
+#include "geometry/taylor_set.h"
 #include "system/impact_system.h"
 #include "evaluation/impact_system_evolver.h"
 #include "output/epsstream.h"
 
-const uint steps = 5;
 
 using namespace Ariadne;
 
@@ -39,21 +39,29 @@ int main() {
   typedef Float64 R;
   typedef ApproximateFloat64 A;
 
-  Evolver<ImpactSystem<R>,Zonotope<R> > evolver;
+  EvolutionParameters<R> evolution_parameters;
+  evolution_parameters.set_maximum_step_size(0.25);
+  Evolver<ImpactSystem<R>,Zonotope<R> > evolver(evolution_parameters);
   std::cout << "evolver = " << evolver << std::endl;
 
+  evolver.verbosity=5;
+
   FunctionInterface<R>* dynamic=new AffineFunction<R>(Vector<R>("[1,0]"),Matrix<R>("[0.0,0.0;-1.0,0.0]"));
-  FunctionInterface<R>* reset=new AffineFunction<R>(Vector<R>("[-0.5,-1.0]"),Matrix<R>("[0.9,0;0,0.9]"));
+  FunctionInterface<R>* reset=new AffineFunction<R>(Vector<R>("[-2,-1.0]"),Matrix<R>("[0.9,0;0,0.9]"));
   FunctionInterface<R>* guard=new AffineFunction<R>(Vector<R>("[-1]"),Matrix<R>("[0,1]"));
 
   ImpactSystem<R> impact_oscillator(*dynamic,*guard,*reset);
 
-  //Box<R> initial_box("[-1.51,-1.49]x[-0.01,+0.01]"); // initial state
+  Box<R> initial_box("[-1.03,-1.01]x[0.49,0.51]"); // initial state
+  //Box<R> initial_box("[-1.11,-1.09]x[0.49,0.51]"); // initial state
+  //Box<R> initial_box("[-1.21,-1.19]x[0.49,0.51]"); // initial state
   //Box<R> initial_box("[-1.51,-1.49]x[0.49,0.51]"); // initial state
-  Box<R> initial_box("[-1.71,-1.69]x[0.49,0.51]"); // initial state
+  //Box<R> initial_box("[-1.81,-1.79]x[0.49,0.51]"); // initial state
+  //Box<R> initial_box("[-1.83,-1.81]x[0.49,0.51]"); // initial state
+  //Box<R> initial_box("[-1.86,-1.84]x[0.49,0.51]"); // initial state
   Zonotope<R> initial_set(initial_box);
-  Rational time=3.5;
-  Integer steps=3;
+  Rational time=1.5;
+  Integer steps=4;
   HybridTime hybrid_time(time,steps);
 
   ListSet< Zonotope<R> > evolve_sets;
@@ -77,8 +85,9 @@ int main() {
   std::cout << "\ninitial:\n";
   std::cout << initial_set << std::endl;
 
+
   epsfstream eps;
-  eps.open("test_impact_system_evolver.eps",Box<R>("[-2.125,2.125]x[-1.25,1.25]"));
+  eps.open("test_impact_system_evolver.eps",Box<R>("[-2.625,1.625]x[-1.25,1.25]"));
   //eps << fill_colour(cyan) << Box<R>("[1,1.25]x[-2,2]");
   eps << fill_colour(cyan) << Box<R>("[-3,3]x[1,2]");
   for(uint i=0; i!=reach_sets.size(); ++i) {
@@ -92,5 +101,6 @@ int main() {
   }
   eps << fill_colour(blue) << initial_set;
   eps.close();
+
 }
 

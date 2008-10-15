90903daaec9785751c24b46dba604b14154a0fab
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2008-10-15 08:03:47 +0000

    Converted ariadne-lite branch to use GNU Autotools for build process.

---
+++ test/test_discrete_evolution.cc
@@ -0,0 +1,129 @@
+/***************************************************************************
+ *            test_discrete_evolution.cc
+ *
+ *  Copyright  2006-8  Pieter Collins
+ *
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <fstream>
+#include <iostream>
+
+#include "tuple.h"
+#include "vector.h"
+#include "matrix.h"
+#include "sparse_differential.h"
+#include "differential_vector.h"
+#include "function.h"
+#include "approximate_taylor_model.h"
+#include "box.h"
+#include "zonotope.h"
+#include "list_set.h"
+#include "evolution_parameters.h"
+#include "hybrid_evolver.h"
+#include "graphics.h"
+#include "logging.h"
+
+#include "models.h"
+
+#include "test.h"
+
+using namespace Ariadne;
+using namespace std;
+using Models::Henon;
+
+class TestDiscreteEvolver
+{
+ public:
+  void test() const;
+};
+
+int main() 
+{
+  TestDiscreteEvolver().test();
+  return ARIADNE_TEST_FAILURES;
+}
+
+void TestDiscreteEvolver::test() const
+{
+  cout << __PRETTY_FUNCTION__ << endl;
+
+  typedef ApproximateTaylorModel EnclosureType;
+  typedef std::pair<DiscreteState,ApproximateTaylorModel> HybridEnclosureType;
+
+  // Set up the evolution parameters and grid
+  Float time(6.0);
+  Float step_size(0.0625);
+  Float grid_size(0.125);
+  Float enclosure_radius(0.25);
+    
+  EvolutionParameters parameters;
+  parameters.maximum_enclosure_radius=enclosure_radius;
+  parameters.maximum_step_size=step_size;
+
+  // Set up the evaluators
+  HybridEvolver evolver;
+  
+  // Define the initial box
+  Box initial_box(2); 
+  initial_box[0]=Interval(1.01,1.02);
+  initial_box[1]=Interval(0.51,0.52);
+
+  cout << "initial_box=" << initial_box << endl;
+
+  // Set up the vector field
+  Vector<Float> p(2); p[0]=1.5; p[1]=0.375;
+  Function<Henon> h(p);
+  cout << "henon_function=" << h << endl;
+  cout << "henon_function.parameters()=" << h.parameters() << endl;
+  cout << h.result_size() << ", " << h.argument_size() << ", " << h.parameter_size() << endl;
+
+  //Function evaluation sanity check
+  cout << "h.evaluate(" << initial_box << ") " << flush; cout << " = " << h.evaluate(initial_box) << endl;
+  cout << "h.jacobian(" << initial_box << ") = " << h.jacobian(initial_box) << endl;
+  cout << endl;
+  
+  // Make a hybrid automaton for the Henon function
+  HybridAutomaton henon("Henon");
+  henon.new_mode(2,IdentityFunction(2));
+
+
+  // Over-approximate the initial set by a grid cell
+  EnclosureType initial_set(initial_box,IdentityFunction(2),4,1);
+  cout << "initial_set=" << initial_set << endl << endl;
+  HybridEnclosureType initial_hybrid_set(DiscreteState(1),initial_set);
+  HybridTime hybrid_time(1.0,5);
+
+  
+  // Compute the reachable sets
+  ListSet<HybridEnclosureType> hybrid_evolve_set,hybrid_reach_set;
+  hybrid_evolve_set = evolver.evolve(henon,initial_hybrid_set,hybrid_time);
+  //cout << "evolve_set=" << hybrid_evolve_set << endl;
+  hybrid_reach_set = evolver.reach(henon,initial_hybrid_set,hybrid_time);
+  //cout << "reach_set=" << hybrid_reach_set << endl;
+  
+  // Print the intial, evolve and reach sets
+  std::ofstream ofs("test_discrete_evolution-henon.eps");
+  //Graphic plot(ofstream);
+  Graphic plot;
+  plot << line_style(true) << fill_colour(cyan) << hybrid_reach_set[1];
+  plot << fill_colour(yellow) << hybrid_evolve_set[1];
+  plot << fill_colour(blue) << initial_set;
+  ofs.close();
+
+}

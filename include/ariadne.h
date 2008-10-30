823fd532fe2ea0a1db1b7d7ff5e32ea7eaa20003
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2008-10-30 11:28:21 +0000

    Added top-level include file ariadne.h.

---
+++ include/ariadne.h
@@ -0,0 +1,54 @@
+/***************************************************************************
+ *            ariadne.h
+ *
+ *  Copyright 2008  Pieter Collins
+ * 
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+ 
+/*! \file ariadne.h
+ *  \brief Top-level header file includes all user headers.
+ */
+
+#ifndef ARIADNE_ARIADNE_H
+#define ARIADNE_ARIADNE_H
+
+#include "numeric.h"
+#include "vector.h"
+#include "matrix.h"
+
+#include "function.h"
+
+#include "function_set.h"
+#include "grid_set.h"
+#include "hybrid_set.h"
+
+#include "point.h"
+#include "box.h"
+#include "polytope.h"
+#include "polyhedron.h"
+
+#include "orbit.h"
+
+#include "hybrid_automaton.h"
+
+#include "hybrid_evolver.h"
+#include "discrete_evolver.h"
+#include "reachability_analyser.h"
+
+#endif

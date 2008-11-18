5230b09ca58aca82973b090188df009c659b9607
Davide Bresolin <davide.bresolin@univr.it>
2008-11-18 15:51:19 +0000

    Addded a simple water tank example.

---
+++ examples/simple.cc
@@ -0,0 +1,263 @@
+/***************************************************************************
+ *            simple.cc
+ *
+ *  Copyright  2008  Davide Bresolin
+ *
+ ****************************************************************************/
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <cstdarg>
+#include "ariadne.h"
+
+using namespace Ariadne;
+
+
+int main() 
+{
+  
+    /// Set the system parameters
+    double a = -0.02;
+    double b = 0.3;
+    double T = 4.0;
+    double hmin = 5.5;
+    double Delta = 0.05;
+    double hmax = 8.0;
+    
+    double A1[4]={a,b,0,0};
+    double b1[2]={0,1.0/T};
+
+    double A2[4]={a,0.0,0.0,0.0};
+    double b2[2]={b,0.0};
+
+    double A3[4]={a,b,0,0};
+    double b3[2]={0,-1.0/T};
+
+    double A4[4]={a,0.0,0.0,0.0};
+    double b4[2]={0.0,0.0};
+
+    /// Build the Hybrid System
+  
+    /// Create a HybridAutomton object
+    HybridAutomaton simple_system;
+  
+    /// Create four discrete states
+    DiscreteState l1(1);
+    DiscreteState l2(2);
+    DiscreteState l3(3);
+    DiscreteState l4(4);
+  
+    /// Create the discrete events
+    DiscreteEvent e12(12);
+    DiscreteEvent e23(23);
+    DiscreteEvent e34(34);
+    DiscreteEvent e41(41);
+  
+    /// Create the dynamics
+    AffineFunction dynamic1(Matrix<Float>(2,2,A1),Vector<Float>(2,b1));
+    AffineFunction dynamic2(Matrix<Float>(2,2,A2),Vector<Float>(2,b2));
+    AffineFunction dynamic3(Matrix<Float>(2,2,A3),Vector<Float>(2,b3));
+    AffineFunction dynamic4(Matrix<Float>(2,2,A4),Vector<Float>(2,b4));
+    
+    cout << "dynamic1 = " << dynamic1 << endl << endl;
+    cout << "dynamic2 = " << dynamic2 << endl << endl;
+    cout << "dynamic3 = " << dynamic3 << endl << endl;
+    cout << "dynamic4 = " << dynamic4 << endl << endl;
+
+    /// Create the resets
+    AffineFunction reset_y_zero(Matrix<Float>(2,2,1.0,0.0,0.0,0.0),Vector<Float>(2,0.0,0.0));
+    cout << "reset_y_zero=" << reset_y_zero << endl << endl;
+    AffineFunction reset_y_one(Matrix<Float>(2,2,1.0,0.0,0.0,0.0),Vector<Float>(2,0.0,1.0));
+    cout << "reset_y_one=" << reset_y_one << endl << endl;
+
+    /// Create the guards.
+    /// Guards are true when f(x) = Ax + b > 0
+    AffineFunction guard12(Matrix<Float>(1,2,0.0,1.0),Vector<Float>(1,-1.0));
+    cout << "guard12=" << guard12 << endl << endl;
+    AffineFunction guard23(Matrix<Float>(1,2,1.0,0.0),Vector<Float>(1,-hmax));
+    cout << "guard23=" << guard23 << endl << endl;
+    AffineFunction guard34(Matrix<Float>(1,2,0.0,-1.0),Vector<Float>(1,0.0));
+    cout << "guard34=" << guard34 << endl << endl;
+    AffineFunction guard41(Matrix<Float>(1,2,-1.0,0.0),Vector<Float>(1,hmin));
+    cout << "guard34=" << guard34 << endl << endl;
+
+    /// Create the invariants.
+    /// Invariants are true when f(x) = Ax + b < 0
+    /// forced transitions do not need an explicit invariant
+//    AffineFunction inv1(Matrix<Float>(1,2,0.0,1.0),Vector<Float>(1,-1.0));
+//    cout << "inv1=" << inv1 << endl << endl;
+//    AffineFunction inv2(Matrix<Float>(1,2,-1.0,0.0),Vector<Float>(1,0.0));
+//    cout << "inv2=" << inv2 << endl << endl;
+  
+    /// Build the automaton
+    simple_system.new_mode(l1,dynamic1);
+    simple_system.new_mode(l2,dynamic2);
+    simple_system.new_mode(l3,dynamic3);
+    simple_system.new_mode(l4,dynamic4);
+  
+    simple_system.new_forced_transition(e12,l1,l2,reset_y_one,guard12);
+    simple_system.new_forced_transition(e23,l2,l3,reset_y_one,guard23);
+    simple_system.new_forced_transition(e34,l3,l4,reset_y_zero,guard34);
+    simple_system.new_forced_transition(e41,l4,l1,reset_y_zero,guard41);
+
+//    simple_system.new_invariant(l2,inv2);
+//    simple_system.new_forced_transition(e21,l2,l1,reset21,guard21);
+    /// Finished building the automaton
+
+    cout << "Automaton = " << simple_system << endl << endl;
+
+    /// Compute the system evolution
+
+    /// Create a HybridEvolver object
+    HybridEvolver evolver;
+
+    /// Set the evolution parameters
+    evolver.parameters().maximum_enclosure_radius = 0.25;
+    evolver.parameters().maximum_step_size = 0.125;
+    std::cout <<  evolver.parameters() << std::endl;
+
+    // Declare the type to be used for the system evolution
+    typedef HybridEvolver::EnclosureType HybridEnclosureType;
+    typedef HybridEvolver::OrbitType OrbitType;
+    typedef HybridEvolver::EnclosureListType EnclosureListType;
+
+    Box initial_box(2, 0.0,0.01, 0.0,0.01);
+    HybridEnclosureType initial_enclosure(l1,initial_box);
+    Box bounding_box(2, -0.1,9.1, -0.1,1.1);
+  
+    HybridTime evolution_time(64.0,6);
+  
+    std::cout << "Computing orbit... " << std::flush;
+    OrbitType orbit = evolver.orbit(simple_system,initial_enclosure,evolution_time,UPPER_SEMANTICS);
+    std::cout << "done." << std::endl;
+
+    std::cout << "Orbit="<<orbit<<std::endl;
+    //plot("tutorial-orbit.png",bounding_box, Colour(0.0,0.5,1.0), orbit.initial());
+    plot("simple-orbit.png",bounding_box, Colour(0.0,0.5,1.0), orbit);
+
+    std::cout << "Computing reach set using HybridEvolver... " << std::flush;
+    EnclosureListType reach = evolver.reach(simple_system,initial_enclosure,evolution_time);
+    std::cout << "done." << std::endl;
+
+    std::cout << "Orbit="<<reach<<std::endl;
+    //plot("tutorial-orbit.png",bounding_box, Colour(0.0,0.5,1.0), orbit.initial());
+    plot("simple-reach-evolver.png",bounding_box, Colour(0.0,0.5,1.0), reach);
+
+
+
+    /// Create a ReachabilityAnalyser object
+    HybridReachabilityAnalyser analyser(evolver);
+    analyser.parameters().lock_to_grid_time = 32.0;
+    analyser.parameters().grid_lengths = 0.05;
+    std::cout <<  analyser.parameters() << std::endl;
+
+    HybridImageSet initial_set;
+    initial_set[l1]=initial_box;
+
+    HybridTime reach_time(64.0,6);
+
+    plot("simple-initial_set.png",bounding_box, Colour(0.0,0.5,1.0), initial_set);
+
+    // Compute evolved sets (i.e. at the evolution time) and reach sets (i.e. up to the evolution time) using lower semantics.
+    // These functions run a bunch of simulations with bounded approximation errors and combines the results.
+    // If the desired evolution time can not be attained without exceeding the error bounds, then the run discarded (without warning)
+    std::cout << "Computing lower evolve set... " << std::flush;
+    HybridGridTreeSet* lower_evolve_set_ptr = analyser.lower_evolve(simple_system,initial_set,reach_time);
+    std::cout << "done." << std::endl;
+    plot("simple-lower_evolve.png",bounding_box, Colour(0.0,0.5,1.0), *lower_evolve_set_ptr);
+
+    std::cout << "Computing lower reach set... " << std::flush;
+    HybridGridTreeSet* lower_reach_set_ptr = analyser.lower_reach(simple_system,initial_set,reach_time);
+    std::cout << "done." << std::endl;
+    plot("simple-lower_reach.png",bounding_box, Colour(0.0,0.5,1.0), *lower_reach_set_ptr);
+
+    // Compute evolved sets and reach sets using upper semantics.
+    // These functions compute over-approximations to the evolved and reachabe sets. Subdivision is used
+    // as necessary to keep the local errors reasonable. The accumulated global error may be very large.
+    std::cout << "Computing upper evolve set... " << std::flush;
+    HybridGridTreeSet* upper_evolve_set_ptr = analyser.upper_evolve(simple_system,initial_set,reach_time);
+    std::cout << "done." << std::endl;
+    plot("simple-upper_evolve.png",bounding_box, Colour(0.0,0.5,1.0), *upper_evolve_set_ptr);
+
+    std::cout << "Computing upper reach set... " << std::flush;
+    HybridGridTreeSet* upper_reach_set_ptr = analyser.upper_reach(simple_system,initial_set,reach_time);
+    std::cout << "done." << std::endl;
+    plot("simple-upper_reach.png",bounding_box, Colour(0.0,0.5,1.0), *upper_reach_set_ptr);
+
+    HybridGridTreeSet* chain_reach_set_ptr = 0;
+    if(false) {
+        std::cout << "Computing chain reach set... " << std::flush;
+        chain_reach_set_ptr = analyser.chain_reach(simple_system,initial_set);
+        std::cout << "done." << std::endl;
+        plot("simple-chain_reach.png",bounding_box, Colour(0.0,0.5,1.0), *chain_reach_set_ptr);
+    } else {
+        std::cerr << "Skipping computation of chain reachable set due to performance issues." << std::endl;
+    }
+/*
+    {
+        // Compute the reach set for times between tlower and tupper. 
+        // The intermediate set is stored to an archive file and used to build the initial set for the reach step
+        // Note that because of peculiarities in the Boost serialization library, 
+        // the object to be serialized must be declared const.
+        Float tlower=0.5; Float tupper=0.75;
+        HybridTime transient_time(tlower,16);
+        HybridTime recurrent_time(tupper-tlower,16);
+    
+        const HybridGridTreeSet* upper_intermediate_set_ptr = analyser.upper_evolve(simple_system,initial_set,transient_time);
+        const HybridGridTreeSet upper_intermediate_set = *upper_intermediate_set_ptr;
+    
+        std::ofstream output_file_stream("tutorial-transient.txt");
+        text_oarchive output_archive(output_file_stream);
+        //output_archive << *upper_intermediate_set_ptr;
+        output_archive << upper_intermediate_set;
+        output_file_stream.close();
+    
+        HybridGridTreeSet rebuilt_upper_intermediate_set;
+    
+        std::ifstream input_file_stream("tutorial-transient.txt");
+        text_iarchive input_archive(input_file_stream);
+        input_archive >> rebuilt_upper_intermediate_set;
+        input_file_stream.close();
+    
+        HybridGridTreeSet* upper_recurrent_set_ptr = analyser.upper_reach(simple_system,initial_set,recurrent_time);
+        plot("tutorial-upper_recurrent.png",bounding_box, Colour(0.0,0.5,1.0), *upper_recurrent_set_ptr);
+    }
+
+
+    {
+        // Use main graphics facilities
+        Graphic g;
+        g.set_bounding_box(Box(2,14,18,0,1));
+        g.set_projection_map(ProjectionFunction(2,0,1));
+
+        g << fill_colour(Colour(1.0,0.0,1.0));
+        g << *upper_reach_set_ptr;
+        g << fill_colour(Colour(0.0,1.0,1.0));
+        g << *lower_reach_set_ptr;
+
+        g << fill_colour(Colour(1.0,1.0,0.0));
+        g << *upper_evolve_set_ptr;
+        g << fill_colour(Colour(1.0,0.0,0.0));
+        g << *lower_evolve_set_ptr;
+
+        g.write("tutorial-all.png");
+
+        // Display the figure in a pop-up window
+        if(false) { g.display(); }
+    }
+*/      
+}

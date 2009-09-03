f6577206eb4075091f077f7212af7c19b170d265
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2009-09-03 20:25:12 +0000

    Committed missing changes to src/expression.cc

--- src/expression.cc
+++ src/expression.cc
@@ -2,7 +2,7 @@
  *            expression.cc
  *
  *  Copyright 2009  Pieter Collins
- *
+ * 
  ****************************************************************************/
 
 /*
@@ -20,7 +20,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
-
+ 
 #include "expression.h"
 #include "valuation.h"
 #include "vector.h"
@@ -56,6 +56,8 @@ class ConstantExpression
     virtual Set<String> arguments() const { return Set<String>(); }
     virtual ConstantExpression<R>* clone() const { return new ConstantExpression<R>(*this); }
     virtual std::ostream& write(std::ostream& os) const { return os << _c; }
+  protected:
+    virtual ExpressionInterface<R>* simplify() const { return this->clone(); }
   private:
     R _c;
 };
@@ -70,11 +72,14 @@ class ConstantExpression<Real>
     ConstantExpression(const Interval& c) :  _c(c) { }
     operator Real() const { return _c; }
     Real value() const { return _c; }
+    virtual String operator_name() const { return "const"; }
     virtual Operator type() const { return CNST; }
     virtual Set<String> arguments() const { return Set<String>(); }
     virtual ConstantExpression<Real>* clone() const { return new ConstantExpression<Real>(*this); }
     virtual std::ostream& write(std::ostream& os) const {
         if(_c.lower()==_c.upper()) { os<<midpoint(_c); } else { os<<_c; } return os; }
+  protected:
+    virtual ExpressionInterface<Real>* simplify() const { return this->clone(); }
   private:
     Real _c;
 };
@@ -90,10 +95,14 @@ class VariableExpression
     VariableExpression(const Variable<R>& v) : _var(v) { }
     const Variable<R>& variable() const { return this->_var; }
     String name() const { return this->_var.name(); }
+    virtual String operator_name() const { return "var"; }
     virtual Operator type() const { return VAR; }
     virtual VariableExpression<R>* clone() const { return new VariableExpression<R>(*this); }
     virtual Set<String> arguments() const { Set<String> r; r.insert(this->name()); return r; }
     virtual std::ostream& write(std::ostream& os) const { return os << this->name(); }
+  protected:
+    virtual ExpressionInterface<R>* simplify() const {
+        return this->clone(); }
   private:
     Variable<R> _var;
 };
@@ -114,10 +123,13 @@ class CoordinateExpression<Real>
     SizeType index() const { return _j; }
     SizeType coordinate() const { return _j; }
     String name() const { String s("x0"); s[1]+=_j; return s; }
+    virtual String operator_name() const { return "coord"; }
     virtual Operator type() const { return IND; }
     virtual CoordinateExpression<Real>* clone() const { return new CoordinateExpression<Real>(*this); }
     virtual Set<String> arguments() const { Set<String> r; r.insert(this->name()); return r; }
     virtual std::ostream& write(std::ostream& os) const { return os << this->name(); }
+  protected:
+    virtual ExpressionInterface<Real>* simplify() const { return this->clone(); }
   private:
     SizeType _as;
     SizeType _j;
@@ -136,16 +148,51 @@ template<class R, class Op=Operator, class A=R> class UnaryExpression
         : _op(op), _arg(const_cast<ExpressionInterface<A>*>(expr)) { }
     UnaryExpression(const Op& op, shared_ptr< const ExpressionInterface<A> > expr)
         : _op(op), _arg(expr) { }
+    virtual String operator_name() const { return name(_op); }
     virtual Operator type() const { return static_cast<Operator>(_op); }
     virtual UnaryExpression<R,Op,A>* clone() const { return new UnaryExpression<R,Op,A>(_op,_arg._ptr); }
     virtual Set<String> arguments() const { return this->_arg.arguments(); }
     virtual std::ostream& write(std::ostream& os) const;
+  protected:
+    virtual ExpressionInterface<R>* simplify() const { return this->clone(); }
+  public:
+    Op _op;
+    Expression<A> _arg;
+};
+
+//! A composed scalar function, based on a standard operator.
+template<class R> class UnaryExpression<R,Operator,R>
+    : public ExpressionInterface<R>
+{
+    typedef Operator Op; typedef R A;
+  public:
+    UnaryExpression(const Op& op, const ExpressionInterface<A>& expr)
+        : _op(op), _arg(expr.clone()) { }
+    UnaryExpression(const Op& op, const ExpressionInterface<A>* expr)
+        : _op(op), _arg(const_cast<ExpressionInterface<A>*>(expr)) { }
+    UnaryExpression(const Op& op, shared_ptr< const ExpressionInterface<A> > expr)
+        : _op(op), _arg(expr) { }
+    virtual String operator_name() const { return name(_op); }
+    virtual Operator type() const { return static_cast<Operator>(_op); }
+    virtual UnaryExpression<R,Op,A>* clone() const { return new UnaryExpression<R,Op,A>(_op,_arg._ptr); }
+    virtual Set<String> arguments() const { return this->_arg.arguments(); }
+    virtual std::ostream& write(std::ostream& os) const;
+  protected:
+    virtual ExpressionInterface<R>* simplify() const { return this->clone(); }
   public:
     Op _op;
     Expression<A> _arg;
 };
 
 template<class R, class Op, class A> inline std::ostream& UnaryExpression<R,Op,A>::write(std::ostream& os) const {
+    switch(_op) {
+        case NEG: return os << '-' << _arg;
+        case NOT: return os << '!' << _arg;
+        default: return os << _op << "(" << _arg << ")";
+    }
+}
+
+template<class R> inline std::ostream& UnaryExpression<R,Operator,R>::write(std::ostream& os) const {
     switch(_op) {
         case NEG: return os << '-' << _arg;
         case NOT: return os << '!' << _arg;
@@ -165,11 +212,67 @@ template<class R, class Op=Operator, class A1=R, class A2=A1> class BinaryExpres
         : _op(op), _arg1(expr1), _arg2(expr2) { }
     BinaryExpression(Op op, shared_ptr< const ExpressionInterface<A1> > expr1, shared_ptr< const ExpressionInterface<A2> > expr2)
         : _op(op), _arg1(expr1), _arg2(expr2)  { }
+    virtual String operator_name() const { return name(_op); }
     virtual Operator type() const { return static_cast<Operator>(_op); }
     virtual BinaryExpression<R,Op,A1,A2>* clone() const { return new BinaryExpression<R,Op,A1,A2>(_op,_arg1._ptr,_arg2._ptr); }
     virtual Set<String> arguments() const { return join(this->_arg1.arguments(),this->_arg2.arguments()); }
     virtual std::ostream& write(std::ostream& os) const {
         return os << "(" << _arg1 << symbol(_op) << _arg2 << ")"; }
+  protected:
+    virtual ExpressionInterface<R>* simplify() const { return this->clone(); }
+  public:
+    Op _op;
+    Expression<A1> _arg1;
+    Expression<A2> _arg2;
+};
+
+
+//! A composed scalar function, based on an arthmetic operator.
+template<class R, class A> class BinaryExpression<R,Comparison,A,A>
+    : public ExpressionInterface<R>
+{
+    typedef Comparison Op; typedef A A1; typedef A A2;
+  public:
+    BinaryExpression(Op op, const ExpressionInterface<A1>& expr1, const ExpressionInterface<A2>& expr2)
+        : _op(op), _arg1(expr1.clone()), _arg2(expr2.clone()) { }
+    BinaryExpression(Op op, const ExpressionInterface<A1>* expr1, const ExpressionInterface<A2>* expr2)
+        : _op(op), _arg1(expr1), _arg2(expr2) { }
+    BinaryExpression(Op op, shared_ptr< const ExpressionInterface<A1> > expr1, shared_ptr< const ExpressionInterface<A2> > expr2)
+        : _op(op), _arg1(expr1), _arg2(expr2)  { }
+    virtual String operator_name() const { return name(_op); }
+    virtual Operator type() const { return static_cast<Operator>(_op); }
+    virtual BinaryExpression<R,Op,A1,A2>* clone() const { return new BinaryExpression<R,Op,A1,A2>(_op,_arg1._ptr,_arg2._ptr); }
+    virtual Set<String> arguments() const { return join(this->_arg1.arguments(),this->_arg2.arguments()); }
+    virtual std::ostream& write(std::ostream& os) const {
+        return os << "(" << _arg1 << symbol(_op) << _arg2 << ")"; }
+  protected:
+    virtual ExpressionInterface<R>* simplify() const { return this->clone(); }
+  public:
+    Op _op;
+    Expression<A1> _arg1;
+    Expression<A2> _arg2;
+};
+
+
+//! An expression in multiple variables of the same type
+template<class R, class Op=Operator, class A=R> class MultiaryExpression
+    : public ExpressionInterface<R>
+{
+  public:
+    MultiaryExpression(Op op, const ExpressionInterface<A>& expr1, const ExpressionInterface<A>& expr2)
+        : _op(op), _args() { _args.append(Expression<A>(expr1.clone())); _args.append(Expression<A>(expr2.clone()));  }
+    MultiaryExpression(Op op, const ExpressionInterface<A>* expr1, const ExpressionInterface<A>* expr2)
+        : _op(op), _args() { _args.append(Expression<A>(expr1)); _args.append(Expression<A>(expr2));  }
+    MultiaryExpression(Op op, shared_ptr< const ExpressionInterface<A> > expr1, shared_ptr< const ExpressionInterface<A> > expr2)
+        : _op(op), _args() { _args.append(expr1); _args.append(expr2);  }
+    virtual unsigned int number_of_arguments() const { return _args.size(); }
+    virtual String operator_name() const { return name(_op); }
+    virtual Operator type() const { return static_cast<Operator>(_op); }
+    virtual MultiaryExpression<R,Op,A>* clone() const { return new MultiaryExpression<R,Op,A>(_op,_args); }
+    virtual Set<String> arguments() const {
+        Set<String> res; for(uint i=0; i!=_args.size(); ++i) { res.adjoin(_args[i].arguments()); } return res; }
+    virtual std::ostream& write(std::ostream& os) const {
+        return os << _op << _args; }
 /*
   private:
     template<class R, class A> inline
@@ -177,9 +280,10 @@ template<class R, class Op=Operator, class A1=R, class A2=A1> class BinaryExpres
 */
   public:
     Op _op;
-    Expression<A1> _arg1;
-    Expression<A2> _arg2;
+    List< Expression<A> > _args;
 };
+
+
 
 bool operator==(const Expression<Tribool>& e, bool v) {
     const ConstantExpression<Tribool>* expr = dynamic_cast<const ConstantExpression<Tribool>*>(e.ptr());
@@ -187,13 +291,63 @@ bool operator==(const Expression<Tribool>& e, bool v) {
 }
 
 
-template<class R> Expression<R>::Expression(const R& c) : _ptr(new ConstantExpression<R>(c)) { }
-template<class R> Expression<R>::Expression(const Constant<R>& c) : _ptr(new ConstantExpression<R>(c.value())) { }
-template<class R> Expression<R>::Expression(const Variable<R>& v) : _ptr(new VariableExpression<R>(v)) { }
+template<class R> Expression<R>::Expression(const R& c)
+    : _ptr(new ConstantExpression<R>(c))
+{
+}
+
+template<class R> Expression<R>::Expression(const Variable<R>& v)
+    : _ptr(new VariableExpression<R>(v))
+{
+}
+
+template<class R> List< Expression<R> > Expression<R>::subexpressions() const {
+    // Return the basic subexpressions used in forming the expression
+    // Ideally, this code should be part of ExpressionInterface to avoid the
+    // switching logic
+    List< Expression<R> > res;
+    const UnaryExpression<R>* uptr;
+    const BinaryExpression<R>* bptr;
+    uptr=dynamic_cast<const UnaryExpression<R>*>(this->_ptr.operator->());
+    if(uptr) {
+        res.append(uptr->_arg);
+    } else {
+        bptr=dynamic_cast<const BinaryExpression<R>*>(this->_ptr.operator->());
+        if(bptr) {
+            res.append(bptr->_arg1);
+            res.append(bptr->_arg2);
+        }
+        else {
+            std::cerr<<"Warning: subexpressions of "<<*this<<"\n";
+        }
+    }
+    return res;
+}
+
+List< Expression<Real> > Expression<Real>::subexpressions() const {
+    // Return the basic subexpressions used in forming the expression
+    // Ideally, this code should be part of ExpressionInterface to avoid the
+    // switching logic
+    List< Expression<R> > res;
+    const UnaryExpression<R>* uptr;
+    const BinaryExpression<R>* bptr;
+    uptr=dynamic_cast<const UnaryExpression<R>*>(this->_ptr.operator->());
+    if(uptr) {
+        res.append(uptr->_arg);
+    } else {
+        bptr=dynamic_cast<const BinaryExpression<R>*>(this->_ptr.operator->());
+        if(bptr) {
+            res.append(bptr->_arg1);
+            res.append(bptr->_arg2);
+        }
+    }
+    return res;
+}
+
+
 
 Expression<Real>::Expression(const double& c) : _ptr(new ConstantExpression<Real>(c)) { }
 Expression<Real>::Expression(const Interval& c) : _ptr(new ConstantExpression<Real>(c)) { }
-Expression<Real>::Expression(const Constant<Real>& c) : _ptr(new ConstantExpression<Real>(c.value())) { }
 Expression<Real>::Expression(const Variable<Real>& v) : _ptr(new VariableExpression<Real>(v)) { }
 
 template class Expression<Boolean>;
@@ -206,13 +360,19 @@ template class Expression<Real>;
 
 
 
-
 template<class R, class Op, class A> inline
 Expression<R> make_expression(Op op, Expression<A> e) {
     return Expression<R>(new UnaryExpression<R,Op,A>(op,e._ptr)); }
 template<class R, class Op, class A1, class A2> inline
 Expression<R> make_expression(Op op, Expression<A1> e1, Expression<A2> e2) {
     return Expression<R>(new BinaryExpression<R,Op,A1,A2>(op,e1._ptr,e2._ptr)); }
+
+template<class R, class Op, class A> inline
+Expression<R> make_expression(Op op, ExpressionInterface<A>* eptr) {
+    return Expression<R>(new UnaryExpression<R,Op,A>(op,eptr)); }
+template<class R, class Op, class A1, class A2> inline
+Expression<R> make_expression(Op op, ExpressionInterface<A1>* e1ptr, ExpressionInterface<A2>* e2ptr) {
+    return Expression<R>(new BinaryExpression<R,Op,A1,A2>(op,e1ptr,e2ptr)); }
 
 
 Expression<Boolean> operator&&(Expression<Boolean> e1, Expression<Boolean> e2) {
@@ -516,8 +676,80 @@ template<class X> X evaluate(const Expression<Real>& e, const Vector<X>& x) {
 template Tribool evaluate(const Expression<Tribool>& e, const ContinuousValuation<Float>& x);
 template Float evaluate(const Expression<Real>& e, const ContinuousValuation<Float>& x);
 
+template<class X, class Y> Expression<X> substitute_variable(const VariableExpression<X>& e, const Variable<Y>& v, const Y& c) {
+    return Expression<X>(e.clone()); }
+template<class X> Expression<X> substitute_variable(const VariableExpression<X>& e, const Variable<X>& v, const X& c) {
+    if(e.variable()==v) { return Expression<X>(c); } else { return Expression<X>(e.clone()); } }
 
 
+template<class X, class Y> Expression<X> substitute(const Expression<X>& e, const Variable<Y>& v, const Y& c) {
+    const ExpressionInterface<X>* eptr=e.ptr();
+    const BinaryExpression<X,Comparison,Y,Y>* aptr=dynamic_cast<const BinaryExpression<X,Comparison,Y,Y>*>(eptr);
+    if(aptr) { return make_expression<X>(aptr->_op,substitute(aptr->_arg1,v,c),substitute(aptr->_arg2,v,c)); }
+    const BinaryExpression<X>* bptr=dynamic_cast<const BinaryExpression<X>*>(eptr);
+    if(bptr) { return make_expression<X>(bptr->_op,substitute(bptr->_arg1,v,c),substitute(bptr->_arg2,v,c)); }
+    const UnaryExpression<X>* uptr=dynamic_cast<const UnaryExpression<X>*>(eptr);
+    if(uptr) { return make_expression<X>(uptr->_op,substitute(uptr->_arg,v,c)); }
+    const ConstantExpression<X>* cptr=dynamic_cast<const ConstantExpression<X>*>(eptr);
+    if(cptr) { return e; }
+    const VariableExpression<X>* vptr=dynamic_cast<const VariableExpression<X>*>(eptr);
+    if(vptr) { return substitute_variable(*vptr,v,c); }
+}
+
+template Expression<Tribool> substitute(const Expression<Tribool>& e, const Variable<Tribool>& v, const Tribool& c);
+template Expression<Tribool> substitute(const Expression<Tribool>& e, const Variable<Real>& v, const Real& c);
+template Expression<Real> substitute(const Expression<Real>& e, const Variable<Real>& v, const Real& c);
+
+template<class X> Expression<X> simplify(const Expression<X>& e) {
+    return e;
+}
+
+
+template<> Expression<Tribool> simplify(const Expression<Tribool>& e) {
+    const ExpressionInterface<Tribool>* eptr=e._ptr.operator->();
+    const UnaryExpression<Tribool>* uptr=dynamic_cast<const UnaryExpression<Tribool>*>(eptr);
+    if(uptr) {
+        Expression<Tribool> sarg=simplify(uptr->_arg);
+        if(uptr->_op==NOT) {
+            const UnaryExpression<Tribool>* nuptr=dynamic_cast<const UnaryExpression<Tribool>*>(sarg._ptr.operator->());
+            if(nuptr && nuptr->_op==NOT) {
+                return nuptr->_arg;
+            }
+            const ConstantExpression<Tribool>* ncptr=dynamic_cast<const ConstantExpression<Tribool>*>(sarg._ptr.operator->());
+            if(ncptr) {
+                return Expression<Tribool>(!ncptr->value());
+            }
+        }
+        return make_expression<Tribool>(uptr->_op,sarg);
+    }
+    const BinaryExpression<Tribool>* bptr=dynamic_cast<const BinaryExpression<Tribool>*>(eptr);
+    if(bptr) {
+        Expression<Tribool> sarg1=simplify(bptr->_arg1);
+        Expression<Tribool> sarg2=simplify(bptr->_arg2);
+        const ConstantExpression<Tribool>* carg1ptr=dynamic_cast<const ConstantExpression<Tribool>*>(sarg1._ptr.operator->());
+        const ConstantExpression<Tribool>* carg2ptr=dynamic_cast<const ConstantExpression<Tribool>*>(sarg2._ptr.operator->());
+        if(carg1ptr && carg2ptr) {
+            if(bptr->_op==AND) { return Expression<Tribool>(carg1ptr->value() && carg2ptr->value()); }
+            if(bptr->_op==OR) { return Expression<Tribool>(carg1ptr->value() || carg2ptr->value()); }
+        } else if(carg1ptr) {
+            if(bptr->_op==AND && carg1ptr->value()==true) { return sarg2; }
+            if(bptr->_op==AND && carg1ptr->value()==false) { return sarg1; }
+            if(bptr->_op==OR && carg1ptr->value()==true) { return sarg1; }
+            if(bptr->_op==OR && carg1ptr->value()==false) { return sarg2; }
+        } else if(carg2ptr) {
+            if(bptr->_op==AND && carg2ptr->value()==true) { return sarg1; }
+            if(bptr->_op==AND && carg2ptr->value()==false) { return sarg2; }
+            if(bptr->_op==OR && carg2ptr->value()==true) { return sarg2; }
+            if(bptr->_op==OR && carg2ptr->value()==false) { return sarg1; }
+        } else {
+            return make_expression<Tribool>(bptr->_op,sarg1,sarg2);
+        }
+    }
+    return e;
+}
+
+template Expression<Real> simplify(const Expression<Real>& e);
+template Expression<Tribool> simplify(const Expression<Tribool>& e);
 
 
 template<class X>

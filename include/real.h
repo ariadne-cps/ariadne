0ed36b8fa26dfbe8e0665cf4c0e202c863120a24
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2011-01-05 10:09:46 +0000

    Changed Real class to be based on compiled rather than inline functions. This will ease transition to a class based on formulae.

--- include/real.h
+++ include/real.h
@@ -50,6 +50,7 @@ class Real;
 template<class X> X pi();
 template<class X> X inf();
 
+class RealBody;
 
 //! \ingroup NumericModule
 //! \brief Computable real numbers.
@@ -60,63 +61,65 @@ class Real {
   public:
     typedef Real NumericType;
   public:
+    //! Destructor.
+    ~Real();
     //! Default constructor yields the exact value \a 0.
-    Real() : _ivl() { }
+    Real();
     //! \brief Construct from a string literal.
     //! This can be used to create real numbers representing decimal values e.g. \c %Real("4.2") .
     explicit Real(const std::string& s);
-    Real(unsigned int m) : _ivl(m) { }
-    Real(int n) : _ivl(n) { }
+    Real(unsigned int m);
+    Real(int n);
     //! \brief Convert from a builtin double-precision floating-point value.
     //! A numeric literal is first processed by the language support, and the resulting %Real may not have
     //! the same value as the mathematical literal. e.g. \c %Real(4.2) has the value 4.2000000000000002 to 16 decimal places.
-    Real(double x) : _ivl(x) { }
+    Real(double x);
 #ifdef HAVE_GMPXX_H
     //! \brief Construct from a rational number.
     Real(const Rational& q);
 #endif
     //! \brief Construct from a floating-point value.
-    explicit Real(const Float& x) : _ivl(x) { }
+    explicit Real(const Float& x);
     //! \brief Construct from a interval. The resulting %Real object does not describe a number arbitrarily accurately.
     //! \deprecated This constructor should be avoided in user code.
-    explicit Real(const Interval& ivl) : _ivl(ivl) { }
-    explicit Real(double l, double u) : _ivl(l,u) { }
-    explicit Real(double l, double x, double u) : _ivl(l,u) { }
-    Real& operator=(const double& x) { this->_ivl=x; return *this; }
-    Real& operator=(const Float& x) { this->_ivl=x; return *this; }
-    Real& operator=(const Interval& x) { this->_ivl=x; return *this; }
+    explicit Real(const Interval& ivl);
+    explicit Real(double l, double u);
+    explicit Real(double l, double x, double u);
+    //! \brief Copy constructor.
+    Real(const Real&);
+    //! \brief Copy assignment.
+    Real& operator=(const Real&);
+    Real& operator=(const double& x);
+    Real& operator=(const Float& x);
+    Real& operator=(const Interval& x);
     // Can't use conversion operators below in g++ since compiler complains
     // about ambiguous conversion to Interval through Interval(Real::operator Float())
     //operator Float() const { return this->_ivl.midpoint(); }
     //operator Interval() const { return this->_ivl; }
   public:
     //! \brief Get an approximation as a builtin double-precision floating-point number.
-    double get_d() const { return this->_ivl.get_d(); }
+    double get_d() const;
   private:
     friend Float::Float(const Real&);
     friend Interval::Interval(const Real&);
 };
 
-inline Float::Float(const Real& x) : v(x._ivl.midpoint().v) { }
-inline Interval::Interval(const Real& x) : l(x._ivl.l), u(x._ivl.u) { }
-inline Float& Float::operator=(const Real& x) { *this=Float(x); return *this; }
-inline Interval& Interval::operator=(const Real& x) { *this=Interval(x); return *this; }
-
 //@{
 //! \related Real \name Arithmetic Operators
 
 //!  \brief Unary plus operator.
-inline Real operator+(const Real& x) { return Real(+static_cast<Interval>(x)); }
+Real operator+(const Real& x);
 //!  \brief Unary negation operator.
-inline Real operator-(const Real& x) { return Real(-static_cast<Interval>(x)); }
+Real operator-(const Real& x);
 //!  \brief Binary addition operator.
-inline Real operator+(const Real& x, const Real& y) { return Real(static_cast<Interval>(x)+static_cast<Interval>(y)); }
+Real operator+(const Real& x, const Real& y);
 //!  \brief Subtraction operator.
-inline Real operator-(const Real& x, const Real& y) { return Real(static_cast<Interval>(x)-static_cast<Interval>(y)); }
+Real operator-(const Real& x, const Real& y);
 //! \brief Binary multiplication operator.
-inline Real operator*(const Real& x, const Real& y) { return Real(static_cast<Interval>(x)*static_cast<Interval>(y)); }
+Real operator*(const Real& x, const Real& y);
 //! \brief Division operator.
-inline Real operator/(const Real& x, const Real& y) { return Real(static_cast<Interval>(x)/static_cast<Interval>(y)); }
+Real operator/(const Real& x, const Real& y);
+
 //!  \brief Inplace addition operator.
 inline Real& operator+=(Real& x, const Real& y) { return x=x+y; }
 //!  \brief Inplace subtraction operator.
@@ -197,39 +200,39 @@ inline tribool operator< (const Real& x, double y) { return static_cast<Interval
 //! \related Real \name Arithmetical, algebraic and transcendental functions
 
 //!  \brief The absolute value function \c |x|.
-inline Real abs(const Real& x) { return Real(abs(static_cast<Interval>(x))); }
+Real abs(const Real& x);
 //!  \brief The unary plus function \c +x.
-inline Real pos(const Real& x) { return Real(pos(static_cast<Interval>(x))); }
+Real pos(const Real& x);
 //!  \brief The unary negation function \c -x.
-inline Real neg(const Real& x) { return Real(neg(static_cast<Interval>(x))); }
+Real neg(const Real& x);
 //!  \brief The reciprocal function \c 1/x.
-inline Real sqr(const Real& x) { return Real(sqr(static_cast<Interval>(x))); }
+Real sqr(const Real& x);
 //!  \brief The reciprocal function \c 1/x.
-inline Real rec(const Real& x) { return Real(rec(static_cast<Interval>(x))); }
+Real rec(const Real& x);
 //!  \brief The binary addition function \c x+y.
-inline Real add(const Real& x, const Real& y) { return Real(add(static_cast<Interval>(x),static_cast<Interval>(y))); }
+Real add(const Real& x, const Real& y);
 //!  \brief The subtraction function \c x-y.
-inline Real sub(const Real& x, const Real& y) { return Real(sub(static_cast<Interval>(x),static_cast<Interval>(y))); }
+Real sub(const Real& x, const Real& y);
 //!  \brief The binary multiplication function \c x*y.
-inline Real mul(const Real& x, const Real& y) { return Real(mul(static_cast<Interval>(x),static_cast<Interval>(y))); }
+Real mul(const Real& x, const Real& y);
 //!  \brief The division function \c x/y.
-inline Real div(const Real& x, const Real& y) { return Real(div(static_cast<Interval>(x),static_cast<Interval>(y))); }
+Real div(const Real& x, const Real& y);
 //!  \brief The integer power function \c x^n.
-inline Real pow(const Real& x, int n) { return Real(pow(static_cast<Interval>(x),n)); }
+Real pow(const Real& x, int n);
 //!  \brief The square-root function.
-inline Real sqrt(const Real& x) { return Real(sqrt(static_cast<Interval>(x))); }
+Real sqrt(const Real& x);
 //!  \brief The exponential function.
-inline Real exp(const Real& x) { return Real(exp(static_cast<Interval>(x))); }
+Real exp(const Real& x);
 //!  \brief The natural logarithm function.
-inline Real log(const Real& x) { return Real(log(static_cast<Interval>(x))); }
+Real log(const Real& x);
 //!  \brief The constant \a pi.
-template<> inline Real pi<>() { return Real(pi<Interval>()); }
+template<> Real pi<>();
 //!  \brief The sine function.
-inline Real sin(const Real& x) { return Real(sin(static_cast<Interval>(x))); }
+Real sin(const Real& x);
 //!  \brief The cosine function.
-inline Real cos(const Real& x) { return Real(cos(static_cast<Interval>(x))); }
+Real cos(const Real& x);
 //!  \brief The tangent function.
-inline Real tan(const Real& x) { return Real(tan(static_cast<Interval>(x))); }
+Real tan(const Real& x);
 
 //@}
 
--- src/real.cc
+++ src/real.cc
@@ -33,9 +33,59 @@
 namespace Ariadne {
 
 
+Real::~Real() { }
+Real::Real() : _ivl() { }
+Real::Real(unsigned int m) : _ivl(m) { }
+Real::Real(int n) : _ivl(n) { }
+Real::Real(double x) : _ivl(x) { }
+
+Real::Real(const Float& x) : _ivl(x) { }
+Real::Real(const Interval& ivl) : _ivl(ivl) { }
+Real::Real(const Real& x) : _ivl(x._ivl) { }
+Real::Real(double l, double u) : _ivl(l,u) { }
+Real::Real(double l, double x, double u) : _ivl(l,u) { }
+Real& Real::operator=(const double& x) { this->_ivl=x; return *this; }
+Real& Real::operator=(const Float& x) { this->_ivl=x; return *this; }
+Real& Real::operator=(const Interval& x) { this->_ivl=x; return *this; }
+Real& Real::operator=(const Real& x) { this->_ivl=x._ivl; return *this; }
+double Real::get_d() const { return this->_ivl.get_d(); }
+
+Float::Float(const Real& x) : v(x._ivl.midpoint().v) { }
+Interval::Interval(const Real& x) : l(x._ivl.l), u(x._ivl.u) { }
+Float& Float::operator=(const Real& x) { *this=Float(x); return *this; }
+Interval& Interval::operator=(const Real& x) { *this=Interval(x); return *this; }
+
+Real operator+(const Real& x) { return Real(+static_cast<Interval>(x)); }
+Real operator-(const Real& x) { return Real(-static_cast<Interval>(x)); }
+Real operator+(const Real& x, const Real& y) { return Real(static_cast<Interval>(x)+static_cast<Interval>(y)); }
+Real operator-(const Real& x, const Real& y) { return Real(static_cast<Interval>(x)-static_cast<Interval>(y)); }
+Real operator*(const Real& x, const Real& y) { return Real(static_cast<Interval>(x)*static_cast<Interval>(y)); }
+Real operator/(const Real& x, const Real& y) { return Real(static_cast<Interval>(x)/static_cast<Interval>(y)); }
+
+Real abs(const Real& x) { return Real(abs(static_cast<Interval>(x))); }
+Real pos(const Real& x) { return Real(pos(static_cast<Interval>(x))); }
+Real neg(const Real& x) { return Real(neg(static_cast<Interval>(x))); }
+Real sqr(const Real& x) { return Real(sqr(static_cast<Interval>(x))); }
+Real rec(const Real& x) { return Real(rec(static_cast<Interval>(x))); }
+Real add(const Real& x, const Real& y) { return Real(add(static_cast<Interval>(x),static_cast<Interval>(y))); }
+Real sub(const Real& x, const Real& y) { return Real(sub(static_cast<Interval>(x),static_cast<Interval>(y))); }
+Real mul(const Real& x, const Real& y) { return Real(mul(static_cast<Interval>(x),static_cast<Interval>(y))); }
+Real div(const Real& x, const Real& y) { return Real(div(static_cast<Interval>(x),static_cast<Interval>(y))); }
+Real pow(const Real& x, int n) { return Real(pow(static_cast<Interval>(x),n)); }
+Real sqrt(const Real& x) { return Real(sqrt(static_cast<Interval>(x))); }
+Real exp(const Real& x) { return Real(exp(static_cast<Interval>(x))); }
+Real log(const Real& x) { return Real(log(static_cast<Interval>(x))); }
+template<> Real pi<>() { return Real(pi<Interval>()); }
+Real sin(const Real& x) { return Real(sin(static_cast<Interval>(x))); }
+Real cos(const Real& x) { return Real(cos(static_cast<Interval>(x))); }
+Real tan(const Real& x) { return Real(tan(static_cast<Interval>(x))); }
+
+
+
 std::ostream& operator<<(std::ostream& os, const Real& x)
 {
     Interval ivl=static_cast<Interval>(x);
+    return os << ivl.midpoint();
     if(ivl.lower()==ivl.upper()) { return os << ivl.lower(); }
     else { return os << ivl; }
     return os << "Real(" << ivl.lower() <<',' << ivl.upper() << ")";

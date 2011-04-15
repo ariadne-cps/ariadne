00e9b8ef8e3429cf264c03a42e4f6b807f7c0298
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2011-04-15 12:02:43 +0000

    Fixed problems with CMake files and added config.h.cmake.

---
+++ config.h.cmake
@@ -0,0 +1,10 @@
+#ifndef ARIADNE_CONFIG_H
+#define ARIADNE_CONFIG_H
+
+#cmakedefine SIZEOF_INT @SIZEOF_INT@
+#cmakedefine HAVE_GMPXX_H
+#cmakedefine HAVE_CAIRO_H
+
+#cmakedefine HAVE_LIBBOOST_SERIALIZATION
+
+#endif /* ARIADNE_CONFIG_H */

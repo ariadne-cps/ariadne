b5c685b6637919c47b06586f50fcd74c3002e18a
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2011-12-02 11:53:21 +0000

    Added partially-implemented AffineModel<Float>.

--- include/affine_model.h
+++ include/affine_model.h
@@ -35,33 +35,39 @@
 #include "macros.h"
 #include "pointer.h"
 
+#include "numeric.h"
 #include "vector.h"
 #include "matrix.h"
 
 namespace Ariadne {
 
 template<class X> class AffineModel;
+template<class X> class ScalarFunction;
+template<class X> class TaylorModel;
+
+AffineModel<Interval> affine_model(const TaylorModel<Interval>& taylor_model);
+AffineModel<Interval> affine_model(const IntervalVector& domain, const ScalarFunction<Interval>& function);
+Vector< AffineModel<Interval> > affine_models(const IntervalVector& domain, const VectorFunction<Interval>& function);
 
 
-//! An affine expression \f$f:\R^n\rightarrow\R\f$ given by \f$f(x)=\sum_{i=0}^{n-1} a_i x_i + b\f$.
+//! An affine expression \f$f:\R^n\rightarrow\R\f$ given by \f$f(x) \approx \sum_{i=0}^{n-1} a_i x_i + b\f$.
 template<>
-class AffineModel<Interval>
+class AffineModel<Float>
 {
   public:
     explicit AffineModel() : _c(), _g() { }
-    explicit AffineModel(uint n) : _c(0.0), _g(n,0.0), _e(0.0) { }
-    explicit AffineModel(const Float& c, const Vector<Float>& g, const Float& e) : _c(c), _g(g), _e(e) { }
-    explicit AffineModel(uint as, double c, double g0, ...) : _c(static_cast<X>(c)), _g(as), _e(0.0) {
-        _g[0]=static_cast<X>(g0); va_list args; va_start(args,g0);
-        for(uint i=1; i!=as; ++i) { _g[i]=static_cast<X>(va_arg(args,double)); } }
+    explicit AffineModel(uint n) : _c(0.0), _g(n,0.0) { }
+    explicit AffineModel(const Float& c, const Vector<Float>& g) : _c(c), _g(g) { }
+    explicit AffineModel(uint as, double c, double g0, ...) : _c(static_cast<Float>(c)), _g(as) {
+        _g[0]=static_cast<Float>(g0); va_list args; va_start(args,g0);
+        for(uint i=1; i!=as; ++i) { _g[i]=static_cast<Float>(va_arg(args,double)); } }
 
-    AffineModel<Interval>& operator=(const Float& c) {
-        this->_c=c; for(uint i=0; i!=this->_g.size(); ++i) { this->_g[i]=0.0; } this->_e=0.0; return *this; }
-    static AffineModel<Interval> constant(uint n, const Float& c) {
-        return AffineModel<Interval>(c,Vector<Float>(n,0.0)); }
-    static AffineModel<Interval> variable(uint n, uint j) {
-        return AffineModel<Interval>(0.0,Vector<Float>::unit(n,j)); }
-
+    AffineModel<Float>& operator=(const Float& c) {
+        this->_c=c; for(uint i=0; i!=this->_g.size(); ++i) { this->_g[i]=0.0; } return *this; }
+    static AffineModel<Float> constant(uint n, const Float& c) {
+        return AffineModel<Float>(c,Vector<Float>(n,0.0)); }
+    static AffineModel<Float> variable(uint n, uint j) {
+        return AffineModel<Float>(0.0,Vector<Float>::unit(n,j)); }
 
     const Vector<Float>& a() const { return this->_g; }
     const Float& b() const { return this->_c; }
@@ -69,7 +75,69 @@ class AffineModel<Interval>
     const Vector<Float>& gradient() const { return this->_g; }
     const Float& gradient(uint i) const { return this->_g[i]; }
     const Float& value() const { return this->_c; }
+
+    uint argument_size() const { return this->_g.size(); }
+  private:
+    Float _c;
+    Vector<Float> _g;
+};
+
+//! \relates AffineModel
+//! \brief Negation of an affine model.
+AffineModel<Float> operator-(const AffineModel<Float>& f);
+//! \relates AffineModel
+//! \brief Addition of two affine models.
+AffineModel<Float> operator+(const AffineModel<Float>& f1, const AffineModel<Float>& f2);
+//! \relates AffineModel
+//! \brief Subtraction of two affine models.
+AffineModel<Float> operator-(const AffineModel<Float>& f1, const AffineModel<Float>& f2);
+//! \relates AffineModel
+//! \brief Multiplication of two affine models.
+AffineModel<Float> operator*(const AffineModel<Float>& f1, const AffineModel<Float>& f2);
+//! \relates AffineModel
+//! \brief Addition of a constant to an affine model.
+AffineModel<Float>& operator+=(AffineModel<Float>& f1, const Float& c2);
+//! \relates AffineModel
+//! \brief Scalar multiplication of an affine model.
+AffineModel<Float>& operator*=(AffineModel<Float>& f1, const Float& c2);
+
+//! \relates AffineModel
+//! \brief Write to an output stream.
+std::ostream& operator<<(std::ostream& os, const AffineModel<Float>& f);
+
+
+//! An affine expression \f$f:[-1,+1]^n\rightarrow\R\f$ given by \f$f(x)=\sum_{i=0}^{n-1} a_i x_i + b \pm e\f$.
+template<>
+class AffineModel<Interval>
+{
+  public:
+    explicit AffineModel() : _c(), _g() { }
+    explicit AffineModel(uint n) : _c(0.0), _g(n,0.0), _e(0.0) { }
+    explicit AffineModel(const Float& c, const Vector<Float>& g, const Float& e) : _c(c), _g(g), _e(e) { }
+    explicit AffineModel(uint as, double c, double g0, ...) : _c(static_cast<Float>(c)), _g(as), _e(0.0) {
+        _g[0]=static_cast<Float>(g0); va_list args; va_start(args,g0);
+        for(uint i=1; i!=as; ++i) { _g[i]=static_cast<Float>(va_arg(args,double)); } }
+
+    AffineModel<Interval>& operator=(const Float& c) {
+        this->_c=c; for(uint i=0; i!=this->_g.size(); ++i) { this->_g[i]=0.0; } this->_e=0.0; return *this; }
+    static AffineModel<Interval> constant(uint n, const Float& c) {
+        return AffineModel<Interval>(c,Vector<Float>(n,0.0),0.0); }
+    static AffineModel<Interval> variable(uint n, uint j) {
+        return AffineModel<Interval>(0.0,Vector<Float>::unit(n,j),0.0); }
+
+
+    const Vector<Float>& a() const { return this->_g; }
+    const Float& b() const { return this->_c; }
+    const Float& e() const { return this->_e; }
+
+    const Vector<Float>& gradient() const { return this->_g; }
+    const Float& gradient(uint i) const { return this->_g[i]; }
+    const Float& value() const { return this->_c; }
     const Float& error() const { return this->_e; }
+
+    void set_value(const Float& c) { _c=c; }
+    void set_gradient(uint j, const Float& g) { _g[j]=g; }
+    void set_error(const Float& e) { _e=e; }
 
     uint argument_size() const { return this->_g.size(); }
   private:
@@ -101,6 +169,9 @@ AffineModel<Interval>& operator*=(AffineModel<Interval>& f1, const Interval& c2)
 //! \brief Write to an output stream.
 std::ostream& operator<<(std::ostream& os, const AffineModel<Interval>& f);
 
+//! \relates AffineModel
+//! \brief Create from a Taylor model.
+AffineModel<Interval> affine_model(const TaylorModel<Interval>& tm);
 
 
 

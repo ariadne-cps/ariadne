bf3d14c69173271dcef6c128db04fa4f5b51d7dc
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2016-06-10 00:12:26 +0200

    Removed instantiations of Algebra generic classes.

--- algebra/algebra.cc
+++ algebra/algebra.cc
@@ -30,43 +30,4 @@
 
 namespace Ariadne {
 
-template GradedAlgebra<ApproximateNumericType> compose(const Series<ApproximateNumericType>&, const GradedAlgebra<ApproximateNumericType>&);
-template GradedAlgebra<ValidatedNumericType> compose(const Series<ValidatedNumericType>&, const GradedAlgebra<ValidatedNumericType>&);
-template GradedAlgebra<EffectiveNumericType> compose(const Series<EffectiveNumericType>&, const GradedAlgebra<EffectiveNumericType>&);
-
-template NormedAlgebra<ApproximateNumericType> rec(const NormedAlgebra<ApproximateNumericType>&);
-template NormedAlgebra<ValidatedNumericType> rec(const NormedAlgebra<ValidatedNumericType>&);
-
-template NormedAlgebra<ApproximateNumericType> sqrt(const NormedAlgebra<ApproximateNumericType>&);
-template NormedAlgebra<ValidatedNumericType> sqrt(const NormedAlgebra<ValidatedNumericType>&);
-
-template NormedAlgebra<ApproximateNumericType> exp(const NormedAlgebra<ApproximateNumericType>&);
-template NormedAlgebra<ValidatedNumericType> exp(const NormedAlgebra<ValidatedNumericType>&);
-
-template NormedAlgebra<ApproximateNumericType> log(const NormedAlgebra<ApproximateNumericType>&);
-template NormedAlgebra<ValidatedNumericType> log(const NormedAlgebra<ValidatedNumericType>&);
-
-template NormedAlgebra<ApproximateNumericType> sin(const NormedAlgebra<ApproximateNumericType>&);
-template NormedAlgebra<ValidatedNumericType> sin(const NormedAlgebra<ValidatedNumericType>&);
-
-template NormedAlgebra<ApproximateNumericType> cos(const NormedAlgebra<ApproximateNumericType>&);
-template NormedAlgebra<ValidatedNumericType> cos(const NormedAlgebra<ValidatedNumericType>&);
-
-template NormedAlgebra<ApproximateNumericType> tan(const NormedAlgebra<ApproximateNumericType>&);
-template NormedAlgebra<ValidatedNumericType> tan(const NormedAlgebra<ValidatedNumericType>&);
-
-/*
-template NormedAlgebra<ApproximateNumericType> asin(const NormedAlgebra<ApproximateNumericType>&);
-template NormedAlgebra<ValidatedNumericType> asin(const NormedAlgebra<ValidatedNumericType>&);
-template NormedAlgebra<EffectiveNumericType> asin(const NormedAlgebra<EffectiveNumericType>&);
-
-template NormedAlgebra<ApproximateNumericType> acos(const NormedAlgebra<ApproximateNumericType>&);
-template NormedAlgebra<ValidatedNumericType> acos(const NormedAlgebra<ValidatedNumericType>&);
-template NormedAlgebra<EffectiveNumericType> acos(const NormedAlgebra<EffectiveNumericType>&);
-
-template NormedAlgebra<ApproximateNumericType> atan(const NormedAlgebra<ApproximateNumericType>&);
-template NormedAlgebra<ValidatedNumericType> atan(const NormedAlgebra<ValidatedNumericType>&);
-template NormedAlgebra<EffectiveNumericType> atan(const NormedAlgebra<EffectiveNumericType>&);
-*/
-
 } // namespace Ariadne

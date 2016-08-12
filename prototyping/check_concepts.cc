2d884b6ad002fa85b40d538318f1bd078e4b6fd4
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2016-08-12 12:53:00 +0200

    Added prototyping/ directory for code used to prototype design elements. Added prototype code for simple numeric classes and concept checking.

---
+++ prototyping/check_concepts.cc
@@ -0,0 +1,7 @@
+#include "./concepts.h"
+
+namespace Ariadne {
+//template class RealConcept<Real>;
+template class RealConcept<RealArchetype>;
+template class ConcreteConcept<ConcreteArchetype>;
+}

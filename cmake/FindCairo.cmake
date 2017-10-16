9315856b845a138ae35c00360cc34aa183168869
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2017-10-17 00:16:13 +0200

    Updated build files to make Boost Serialization, Cairo and GTK optional. Added sublibraries for faster builds. Addes tests, examples and examples-experimental targets.

--- cmake/FindCairo.cmake
+++ cmake/FindCairo.cmake
@@ -7,52 +7,51 @@
 #  CAIRO_LIBRARIES - Link these to use Cairo
 #
 # Copyright (C) 2007, 2010, Pino Toscano, <pino@kde.org>
+# Copyright (C) 2017, Pieter Collins <pieter.collins@maastrichtuniversity.nl>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 
 if(CAIRO_INCLUDE_DIRS AND CAIRO_LIBRARIES)
+  message("Already found!")
 
   # in cache already
   set(CAIRO_FOUND TRUE)
 
 else(CAIRO_INCLUDE_DIRS AND CAIRO_LIBRARIES)
 
-if(NOT WIN32)
-  # use pkg-config to get the directories and then use these values
-  # in the FIND_PATH() and FIND_LIBRARY() calls
-  find_package(PkgConfig REQUIRED)
-  if(Cairo_FIND_VERSION_COUNT GREATER 0)
-    set(_cairo_version_cmp ">=${Cairo_FIND_VERSION}")
-  endif(Cairo_FIND_VERSION_COUNT GREATER 0)
-  pkg_check_modules(_pc_cairo cairo${_cairo_version_cmp})
+  if(NOT WIN32)
+    # use pkg-config to get the directories and then use these values
+    # in the FIND_PATH() and FIND_LIBRARY() calls
+    find_package(PkgConfig REQUIRED)
+    if(Cairo_FIND_VERSION_COUNT GREATER 0)
+      set(_cairo_version_cmp ">=${Cairo_FIND_VERSION}")
+    endif()
+    pkg_check_modules(_pc_cairo cairo${_cairo_version_cmp})
+  else()
+    # assume so, for now
+    set(_pc_cairo_FOUND TRUE)
+  endif()
+
   if(_pc_cairo_FOUND)
-    set(CAIRO_FOUND TRUE)
-  endif(_pc_cairo_FOUND)
-else(NOT WIN32)
-  # assume so, for now
-  set(CAIRO_FOUND TRUE)
-endif(NOT WIN32)
+    # set it back as false
+    set(CAIRO_FOUND FALSE)
 
-if(CAIRO_FOUND)
-  # set it back as false
-  set(CAIRO_FOUND FALSE)
+    find_library(CAIRO_LIBRARY cairo
+                 HINTS ${_pc_cairo_LIBRARY_DIRS}
+    )
+    set(CAIRO_LIBRARIES "${CAIRO_LIBRARY}")
 
-  find_library(CAIRO_LIBRARY cairo
-               HINTS ${_pc_cairo_LIBRARY_DIRS}
-  )
-  set(CAIRO_LIBRARIES "${CAIRO_LIBRARY}")
+    find_path(CAIRO_INCLUDE_DIR cairo.h
+              HINTS ${_pc_cairo_INCLUDE_DIRS}
+              PATH_SUFFIXES cairo
+    )
+    set(CAIRO_INCLUDE_DIRS "${CAIRO_INCLUDE_DIR}")
 
-  find_path(CAIRO_INCLUDE_DIR cairo.h
-            HINTS ${_pc_cairo_INCLUDE_DIRS}
-            PATH_SUFFIXES cairo
-  )
-  set(CAIRO_INCLUDE_DIRS "${CAIRO_INCLUDE_DIR}")
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(Cairo DEFAULT_MSG CAIRO_LIBRARIES CAIRO_INCLUDE_DIRS)
+  endif()
 
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(Cairo DEFAULT_MSG CAIRO_LIBRARIES CAIRO_INCLUDE_DIRS)
-endif(CAIRO_FOUND)
-
-endif(CAIRO_INCLUDE_DIRS AND CAIRO_LIBRARIES)
+endif()
 
 mark_as_advanced(
   CAIRO_CFLAGS

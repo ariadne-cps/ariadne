774cc81154789e769159a6a67fd251884097b840
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2017-10-21 12:00:49 +0200

    Completed creation of system and iteration by affine sets in SSV tutorial.

--- examples/tutorial-ssv.py
+++ examples/tutorial-ssv.py
@@ -3,20 +3,51 @@
 from ariadne import *
 print dir()
 
-a=Real(Rational(3,2))
+print
+
+a=Real(Rational(8,5))
+b=Rational(1,3)
 x=RealVariable("x")
 y=RealVariable("y")
-z=RealVariable("z")
 
-B=RealVariablesBox([x|{2:3},y|{3:4},4<=(z<=5)])
+f=make_function([x,y],[a-x*x-b*y,x])
+print "f:",f
 
-#print make_set(B)
-print make_box([x,y,z],B)
-print make_set([z,x,y],B)
+e=Dyadic(1,4) # 1/2^4
+bx0=make_box([x,y],[x|{1-e:1+e},(-e<=y)<=e])
+print "bx0:",bx0
 
-c1=ContinuousPredicate(x*y<=a)
-print c1
-#C=RealExpressionConstraintSet([ContinuousPredicate(x*y<=a)])
-#C=RealExpressionConstraintSet([x*y<=1, x+z>=2, (x**2 + y**2) <= 5])
-#print C
+ss=make_set([x,y],[x*x+y*y<=2])
+print "ss:",ss
 
+z0=ValidatedAffineConstrainedImageSet(bx0)
+print "z0:",z0
+
+z1=image(z0,f)
+bbx1=z1.bounding_box()
+print "z1:",z1,bbx1
+z2=image(z1,f)
+print "z2:",z2
+
+
+bx1=ExactBox([{FloatDPValue(0.375):FloatDPValue(0.875)},{FloatDPValue(0.875):FloatDPValue(1.125)}])
+print "separated(z1,bx1):",z1.separated(bx1)
+print "inside(z1,bx1):",z1.inside(bx1)
+print "separated(z2,bx1):",z2.separated(bx1)
+print "inside(z2,bx1):",z2.inside(bx1)
+
+fig=Figure()
+fig.set_bounding_box([{-0.5:1.5},{-0.5:1.5}])
+fig.set_fill_colour(1,1,1)
+fig.draw(bx1)
+fig.set_fill_colour(0.5,0,0)
+fig.draw(image(bbx1,f))
+fig.set_fill_colour(1,0,0)
+fig.draw(z0.bounding_box())
+fig.draw(z1.bounding_box())
+fig.draw(z2.bounding_box())
+fig.set_fill_colour(1,1,0)
+fig.draw(z0)
+fig.draw(z1)
+fig.draw(z2)
+fig.write("tutorial-ssv.png")

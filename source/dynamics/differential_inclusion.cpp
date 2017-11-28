5b73821aeb0f1360fa50a0007aea9c6dfc77efc5
Luca Geretti <luca.geretti@gmail.com>
2017-11-28 14:26:04 +0100

    Imported flow bounds from IntegratorBase

--- source/dynamics/differential_inclusion.cpp
+++ source/dynamics/differential_inclusion.cpp
@@ -149,6 +149,87 @@ List<ValidatedVectorFunctionModelDP> InclusionIntegratorBase::flow(EffectiveVect
 
 
 Pair<PositiveFloatDPValue,UpperBoxType> InclusionIntegratorBase::flow_bounds(ValidatedVectorFunction f, UpperBoxType V, BoxDomainType D, PositiveFloatDPApproximation hsug) const {
+
+    // Set up constants of the method.
+    // TODO: Better estimates of constants
+    const FloatDPValue INITIAL_MULTIPLIER=2.0_exact;
+    const FloatDPValue MULTIPLIER=1.125_exact;
+    const FloatDPValue BOX_RADIUS_MULTIPLIER=1.25_exact;
+    const FloatDPValue BOX_RADIUS_WIDENING=0.25_exact;
+    const Nat EXPANSION_STEPS=4;
+    const Nat REDUCTION_STEPS=8;
+    const Nat REFINEMENT_STEPS=4;
+
+    Vector<FloatDPBounds> const& dx=cast_singleton(D);
+
+    //Vector<ValidatedNumericType> delta=(dx-midpoint(domx))*BOX_RADIUS_WIDENING;
+    Vector<UpperIntervalType> delta=(D-midpoint(D))*BOX_RADIUS_WIDENING;
+
+    // Compute the Lipschitz constant over the initial box
+    FloatDPUpperBound lip = norm(f.jacobian(dx)).upper();
+    //FloatDPUpperBound hlip = 1/lip;
+
+    FloatDPValue h=cast_exact(hsug);
+    FloatDPValue hmin=h*two_exp(-REDUCTION_STEPS);
+    //h=max(hmin,min(hlip,h));
+    //ARIADNE_LOG(4,"L="<<lip<<", hL="<<hlip<<"\n");
+
+    UpperBoxType bx,nbx;
+    Vector<UpperIntervalType> df;
+    UpperIntervalType ih(0,h);
+
+    Bool success=false;
+    while(!success) {
+        ARIADNE_ASSERT_MSG(h>=hmin," h="<<h<<", hmin="<<hmin);
+        //bx=domx+INITIAL_MULTIPLIER*ih*evaluate(vf,domx)+delta;
+        bx=D+INITIAL_MULTIPLIER*ih*f.evaluate(dx)+delta;
+        for(Nat i=0; i!=EXPANSION_STEPS; ++i) {
+            df=apply(f,bx);
+            nbx=D+delta+ih*df;
+            ARIADNE_LOG(7,"h="<<h<<" nbx="<<nbx<<" bx="<<bx<<"\n");
+            if(not definitely(is_bounded(nbx))) {
+                success=false;
+                break;
+            } else if(refines(nbx,bx)) {
+                success=true;
+                break;
+            } else {
+                bx=D+delta+MULTIPLIER*ih*df;
+            }
+        }
+        if(!success) {
+            h=hlf(h);
+            ih=UpperIntervalType(0,h);
+        }
+    }
+
+    ARIADNE_ASSERT(refines(nbx,bx));
+
+    Vector<UpperIntervalType> fbx;
+    fbx=apply(f,bx);
+
+    for(Nat i=0; i!=REFINEMENT_STEPS; ++i) {
+        bx=nbx;
+        fbx=apply(f,bx);
+        nbx=D+delta+ih*fbx;
+        ARIADNE_ASSERT_MSG(refines(nbx,bx),std::setprecision(20)<<"refinement "<<i<<": "<<nbx<<" is not a inside of "<<bx);
+    }
+
+
+    // Check result of operation
+    // We use subset rather than inner subset here since the bound may touch
+    ARIADNE_ASSERT(refines(nbx,bx));
+
+    bx=nbx;
+
+
+    ARIADNE_ASSERT(refines(D,bx));
+
+    ARIADNE_ASSERT_MSG(refines(D+ih*apply(f,bx),bx),
+                       "d="<<dx<<"\nh="<<h<<"\nf(b)="<<apply(f,bx)<<"\nd+hf(b)="<<(D+ih*apply(f,bx))<<"\nb="<<bx<<"\n");
+
+    return std::make_pair(PositiveFloatDPValue(h),bx);
+    /*
     //! Compute a bound B for the differential inclusion dot(x) in f(x)+V for x(0) in D for step size h;
     ARIADNE_LOG(3,"D:"<<D);
 
@@ -166,6 +247,7 @@ Pair<PositiveFloatDPValue,UpperBoxType> InclusionIntegratorBase::flow_bounds(Val
     }
     ARIADNE_LOG(3,"B:"<<B);
     return std::make_pair(h,B);
+     */
 }
 
 Tuple<FloatDPError,FloatDPError,FloatDPError,FloatDPUpperBound>

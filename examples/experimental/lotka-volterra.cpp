60e4ac09669d52230ce7b6f646e890f489202c86
Luca Geretti <luca.geretti@gmail.com>
2018-02-02 16:21:01 +0100

    Added example of Lotka-Volterra system (without noise)

--- examples/experimental/lotka-volterra.cpp
+++ examples/experimental/lotka-volterra.cpp
@@ -30,10 +30,11 @@ using namespace Ariadne;
 int main()
 {
 
-    RealConstant mu("mu",1.0_dec);
-    RealVariable x("x"), y("y");
+    RealConstant u1("u1",3.0_dec);
+    RealConstant u2("u2",1.0_dec);
+    RealVariable x1("x1"), x2("x2");
 
-    VectorField dynamics({dot(x)=y, dot(y)= mu*y*(1-sqr(x))-x});
+    VectorField dynamics({dot(x1)=u1*x1*(1-x2), dot(x2)= u2*x2*(x1-1)});
 
     MaximumError max_err=0.01;
     TaylorSeriesIntegrator integrator(max_err);
@@ -43,35 +44,21 @@ int main()
     evolver.configuration().maximum_enclosure_radius(1.0);
     evolver.configuration().maximum_step_size(0.0625);
     evolver.configuration().maximum_spacial_error(0.0001);
-    evolver.verbosity = 0;
+    evolver.verbosity = 1;
     std::cout <<  evolver.configuration() << std::endl;
 
-    Real x0(2.01);
+    Real x1_0(1.2);
+    Real x2_0(1.1);
     Real eps = 1/1024_q;
 
-    Box<RealInterval> initial_set({{x0-eps,x0+eps},{-eps,eps}});
+    Box<RealInterval> initial_set({{x1_0-eps,x1_0+eps},{x2_0-eps,x2_0+eps}});
 
     std::cout << "Initial set: " << initial_set << std::endl;
-    Real evolution_time(6.68);
+    Real evolution_time(3.6);
 
     std::cout << "Computing orbit... " << std::flush;
     auto orbit = evolver.orbit(evolver.enclosure(initial_set),evolution_time,UPPER_SEMANTICS);
     std::cout << "done." << std::endl;
-/*
-    plot("vanderpol-vf",Axes2d(-2.1,x,2.1, -3.0,y,3.0), Colour(0.0,0.5,1.0), orbit);
 
-    std::cout << "Discretising orbit" << std::flush;
-    Grid grid(2);
-    GridTreeSet gts(grid);
-
-    for (ListSet<Enclosure>::ConstIterator it = orbit.reach().begin(); it != orbit.reach().end(); it++)
-    {
-        std::cout<<"."<<std::flush;
-        it->state_auxiliary_set().adjoin_outer_approximation_to(gts,4);
-    }
-    std::cout << "done." << std::endl;
-
-    // The following currently fails since auxiliary variables are not tracked
-    plot("vanderpol-vf-reach",ApproximateBoxType({{-2.1,2.1}, {-3.0,3.0}}), Colour(0.0,0.5,1.0), gts);
-*/
+    plot("lotka-volterra",ApproximateBoxType({{0.5,1.5}, {0.5,1.5}}), Colour(1.0,0.75,0.5), orbit);
 }

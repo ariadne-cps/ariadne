116051c2103f38daad0660308385938e26f904fe
Luca Geretti <luca.geretti@gmail.com>
2018-09-05 16:37:51 +0200

    Fixed typo on licence header

--- python/ariadne_module.cpp
+++ python/ariadne_module.cpp
@@ -11,7 +11,7 @@
  *  Ariadne is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
- *  (moduleat your option) any later version.
+ *  (at your option) any later version.
  *
  *  Ariadne is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of

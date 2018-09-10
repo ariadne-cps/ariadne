9e8f1031b45a725359fe623c2e1e913ef6d8ff34
Luca Geretti <luca.geretti@gmail.com>
2018-09-11 00:03:27 +0200

    Removed redundant declaration in vector field evolver

--- source/dynamics/vector_field_evolver.cpp
+++ source/dynamics/vector_field_evolver.cpp
@@ -65,8 +65,6 @@ template<class ES> List<ES> subdivide(const ES& enclosure) {
 
 namespace Ariadne {
 
-EffectiveVectorFunction make_function(RealSpace const&, Vector<RealExpression> const&);
-
 VectorField::VectorField(List<DottedRealAssignment> const& dynamics)
     : _variable_names(variable_names(left_hand_sides(dynamics)))
     , _function(make_function(left_hand_sides(dynamics),Vector<RealExpression>(right_hand_sides(dynamics))))

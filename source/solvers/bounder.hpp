8aa4e65c8e6247d694cd9c0655b4ba7c95fd1294
Luca Geretti <luca.geretti@gmail.com>
2018-09-20 22:54:50 +0200

    Upgrade the flow bounding method to a bounder, rename the related classes

--- source/solvers/bounder.hpp
+++ source/solvers/bounder.hpp
@@ -38,85 +38,85 @@
 
 namespace Ariadne {
 
-class FlowBoundsMethodHandlerFactory;
+class BounderFactory;
 
-class FlowBoundsMethodHandlerInterface {
+class BounderInterface {
   public:
-    virtual UpperBoxType initial(ValidatedVectorFunction f, BoxDomainType dom, PositiveFloatDPValue h) const = 0;
-    virtual UpperBoxType refinement(UpperBoxType B, ValidatedVectorFunction f, BoxDomainType dom, PositiveFloatDPValue h) const = 0;
+    virtual Pair<PositiveFloatDPValue,UpperBoxType> flow_bounds(ValidatedVectorFunction f, BoxDomainType dom, PositiveFloatDPApproximation hsug) const = 0;
 };
 
-class FlowBoundsMethodHandlerBase : public FlowBoundsMethodHandlerInterface {
+class BounderBase : public BounderInterface {
   public:
-    virtual UpperBoxType initial(ValidatedVectorFunction f, BoxDomainType dom, PositiveFloatDPValue h) const;
-    virtual UpperBoxType refinement(UpperBoxType B, ValidatedVectorFunction f, BoxDomainType dom, PositiveFloatDPValue h) const;
+    Pair<PositiveFloatDPValue,UpperBoxType> flow_bounds(ValidatedVectorFunction f, BoxDomainType dom, PositiveFloatDPApproximation hsug) const;
   protected:
     virtual UpperBoxType formula(BoxDomainType D, BoxDomainType V, ValidatedVectorFunction f, UpperBoxType B, PositiveFloatDPValue h) const = 0;
-  public:
-    virtual ~FlowBoundsMethodHandlerBase() = default;
-};
-
-class EulerFlowBoundsHandler : public FlowBoundsMethodHandlerBase {
-  protected:
-    virtual UpperBoxType formula(BoxDomainType D, BoxDomainType V, ValidatedVectorFunction f, UpperBoxType B, PositiveFloatDPValue h) const;
-  public:
-    virtual ~EulerFlowBoundsHandler() = default;
-};
-
-class HeunFlowBoundsHandler : public FlowBoundsMethodHandlerBase {
-  protected:
-    virtual UpperBoxType formula(BoxDomainType D, BoxDomainType V, ValidatedVectorFunction f, UpperBoxType B, PositiveFloatDPValue h) const;
-  public:
-    virtual ~HeunFlowBoundsHandler() = default;
-};
-
-class RalstonFlowBoundsHandler : public FlowBoundsMethodHandlerBase {
-  protected:
-    virtual UpperBoxType formula(BoxDomainType D, BoxDomainType V, ValidatedVectorFunction f, UpperBoxType B, PositiveFloatDPValue h) const;
-  public:
-    virtual ~RalstonFlowBoundsHandler() = default;
-};
-
-class RungeKutta4FlowBoundsHandler : public FlowBoundsMethodHandlerBase {
-  protected:
-    virtual UpperBoxType formula(BoxDomainType D, BoxDomainType V, ValidatedVectorFunction f, UpperBoxType B, PositiveFloatDPValue h) const;
-  public:
-    virtual ~RungeKutta4FlowBoundsHandler() = default;
-};
-
-class FlowBoundsMethodHandler : public FlowBoundsMethodHandlerInterface {
-    friend class FlowBoundsMethodHandlerFactory;
   private:
-    SharedPointer<FlowBoundsMethodHandlerInterface> _impl;
-    FlowBoundsMethodHandler(SharedPointer<FlowBoundsMethodHandlerInterface> const& impl) : _impl(impl) { }
+    UpperBoxType _initial(ValidatedVectorFunction f, BoxDomainType dom, PositiveFloatDPValue h) const;
+    UpperBoxType _refinement(UpperBoxType B, ValidatedVectorFunction f, BoxDomainType dom, PositiveFloatDPValue h) const;
   public:
-    FlowBoundsMethodHandler(FlowBoundsMethodHandler const& other) : _impl(other._impl) { }
-    FlowBoundsMethodHandler& operator=(FlowBoundsMethodHandler const& other) { _impl = other._impl; return *this; }
-
-    virtual UpperBoxType initial(ValidatedVectorFunction f, BoxDomainType dom, PositiveFloatDPValue h) const {
-        return _impl->initial(f,dom,h); }
-    virtual UpperBoxType refinement(UpperBoxType B, ValidatedVectorFunction f, BoxDomainType dom, PositiveFloatDPValue h) const {
-        return _impl->refinement(B,f,dom,h); }
-  public:
-    virtual ~FlowBoundsMethodHandler() = default;
+    virtual ~BounderBase() = default;
 };
 
-enum class FlowBoundsMethod : std::uint8_t { EULER, HEUN, RALSTON, RUNGEKUTTA4 };
+class EulerBounder : public BounderBase {
+  protected:
+    virtual UpperBoxType formula(BoxDomainType D, BoxDomainType V, ValidatedVectorFunction f, UpperBoxType B, PositiveFloatDPValue h) const;
+  public:
+    virtual ~EulerBounder() = default;
+};
 
-inline std::ostream& operator << (std::ostream& os, const FlowBoundsMethod& kind) {
+class HeunBounder : public BounderBase {
+  protected:
+    virtual UpperBoxType formula(BoxDomainType D, BoxDomainType V, ValidatedVectorFunction f, UpperBoxType B, PositiveFloatDPValue h) const;
+  public:
+    virtual ~HeunBounder() = default;
+};
+
+class RalstonBounder : public BounderBase {
+  protected:
+    virtual UpperBoxType formula(BoxDomainType D, BoxDomainType V, ValidatedVectorFunction f, UpperBoxType B, PositiveFloatDPValue h) const;
+  public:
+    virtual ~RalstonBounder() = default;
+};
+
+class RungeKutta4Bounder : public BounderBase {
+  protected:
+    virtual UpperBoxType formula(BoxDomainType D, BoxDomainType V, ValidatedVectorFunction f, UpperBoxType B, PositiveFloatDPValue h) const;
+  public:
+    virtual ~RungeKutta4Bounder() = default;
+};
+
+class BounderHandler : public BounderInterface {
+    friend class BounderFactory;
+  private:
+    SharedPointer<BounderInterface> _impl;
+    BounderHandler(SharedPointer<BounderInterface> const& impl) : _impl(impl) { }
+  public:
+    BounderHandler(BounderHandler const& other) : _impl(other._impl) { }
+    BounderHandler& operator=(BounderHandler const& other) { _impl = other._impl; return *this; }
+
+    virtual Pair<PositiveFloatDPValue,UpperBoxType> flow_bounds(ValidatedVectorFunction f, BoxDomainType dom, PositiveFloatDPApproximation hsug) const {
+        return _impl->flow_bounds(f,dom,hsug);
+    }
+  public:
+    virtual ~BounderHandler() = default;
+};
+
+enum class Bounder : std::uint8_t { EULER, HEUN, RALSTON, RUNGEKUTTA4 };
+
+inline std::ostream& operator << (std::ostream& os, const Bounder& kind) {
     switch (kind) {
-        case FlowBoundsMethod::EULER: os << "EULER"; break;
-        case FlowBoundsMethod::HEUN: os << "HEUN"; break;
-        case FlowBoundsMethod::RALSTON: os << "RALSTON"; break;
-        case FlowBoundsMethod::RUNGEKUTTA4: os << "RUNGEKUTTA4"; break;
-        default: ARIADNE_FAIL_MSG("Unhandled flow bounds method for output streaming\n");
+        case Bounder::EULER: os << "EULER"; break;
+        case Bounder::HEUN: os << "HEUN"; break;
+        case Bounder::RALSTON: os << "RALSTON"; break;
+        case Bounder::RUNGEKUTTA4: os << "RUNGEKUTTA4"; break;
+        default: ARIADNE_FAIL_MSG("Unhandled bounder method for output streaming\n");
     }
     return os;
 }
 
-class FlowBoundsMethodHandlerFactory {
+class BounderFactory {
 public:
-    static FlowBoundsMethodHandler create(FlowBoundsMethod method);
+    static BounderHandler create(Bounder method);
 };
 
 

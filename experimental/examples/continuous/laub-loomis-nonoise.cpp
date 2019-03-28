252fd6fea5adeb02af08f0aebabd9024ae2895bf
Luca Geretti <luca.geretti@gmail.com>
2019-03-28 10:08:34 +0100

    Modify Laub-Loomis model for ARCH

--- experimental/examples/continuous/laub-loomis-nonoise.cpp
+++ experimental/examples/continuous/laub-loomis-nonoise.cpp
@@ -24,33 +24,23 @@
 
 #include <cstdarg>
 #include "ariadne.hpp"
+#include "utility/stopwatch.hpp"
 
 using namespace Ariadne;
 
 
-int main()
-{
-    RealVariable x1("x1"),x2("x2"),x3("x3"),x4("x4"),x5("x5"),x6("x6"),x7("x7"),t("t");
+int main() {
+    RealVariable x1("x1"), x2("x2"), x3("x3"), x4("x4"), x5("x5"), x6("x6"), x7("x7"), t("t");
 
-    VectorField dynamics({dot(x1)=Real(1.4)*x3-Real(0.9)*x1,
-                          dot(x2)=Real(2.5)*x5-Real(1.5)*x2,
-                          dot(x3)=Real(0.6)*x7-Real(0.8)*x2*x3,
-                          dot(x4)=Real(2)-Real(1.3)*x3*x4,
-                          dot(x5)=Real(0.7)*x1-x4*x5,
-                          dot(x6)=Real(0.3)*x1-Real(3.1)*x6,
-                          dot(x7)=Real(1.8)*x6-Real(1.5)*x2*x7,
-                          dot(t)=Real(1.0)
+    VectorField dynamics({dot(x1) = Real(1.4) * x3 - Real(0.9) * x1,
+                                 dot(x2) = Real(2.5) * x5 - Real(1.5) * x2,
+                                 dot(x3) = Real(0.6) * x7 - Real(0.8) * x2 * x3,
+                                 dot(x4) = Real(2) - Real(1.3) * x3 * x4,
+                                 dot(x5) = Real(0.7) * x1 - x4 * x5,
+                                 dot(x6) = Real(0.3) * x1 - Real(3.1) * x6,
+                                 dot(x7) = Real(1.8) * x6 - Real(1.5) * x2 * x7,
+                                 dot(t) = Real(1.0)
                          });
-
-    MaximumError max_err=2e-3;
-    TaylorSeriesIntegrator integrator(max_err,Order(4u));
-
-    VectorFieldEvolver evolver(dynamics,integrator);
-    evolver.configuration().maximum_enclosure_radius(0.09);
-    evolver.configuration().maximum_step_size(0.2);
-    evolver.configuration().maximum_spacial_error(2e-3);
-    evolver.verbosity = 1;
-    std::cout <<  evolver.configuration() << std::endl;
 
     Real x1_0(1.2);
     Real x2_0(1.05);
@@ -60,21 +50,113 @@ int main()
     Real x6_0(0.1);
     Real x7_0(0.45);
     Real t_0(0.0);
-    Real eps = 1/10_q;
 
-    Box<RealInterval> initial_set({{x1_0-eps,x1_0+eps},{x2_0-eps,x2_0+eps},{x3_0-eps,x3_0+eps},{x4_0-eps,x4_0+eps},{x5_0-eps,x5_0+eps},{x6_0-eps,x6_0+eps},{x7_0-eps,x7_0+eps},{t_0,t_0}});
+    ListSet<Enclosure> reach1, reach2, reach3;
+/*
+    {
+        MaximumError max_err = 2e-3;
+        TaylorSeriesIntegrator integrator(max_err, Order(3u));
 
-    std::cout << "Initial set: " << initial_set << std::endl;
-    Real evolution_time(20.0);
+        VectorFieldEvolver evolver(dynamics, integrator);
+        evolver.configuration().maximum_enclosure_radius(0.09);
+        evolver.configuration().maximum_step_size(0.2);
+        evolver.configuration().maximum_spacial_error(1e-3);
+        evolver.verbosity = 1;
+        std::cout << evolver.configuration() << std::endl;
 
-    std::cout << "Computing orbit... " << std::flush;
-    auto orbit = evolver.orbit(evolver.enclosure(initial_set),evolution_time,Semantics::UPPER);
-    std::cout << "done." << std::endl;
+        Real eps = 1 / 100_q;
 
-    for (auto set : orbit.reach()) {
-        if (definitely(set.bounding_box()[3].upper().raw() >= 5.0))
-            std::cout << "set with upper value " << set.bounding_box()[3].upper().raw() << " is outside the safe set." << std::endl;
+        Box<RealInterval> initial_set({{x1_0 - eps, x1_0 + eps},
+                                       {x2_0 - eps, x2_0 + eps},
+                                       {x3_0 - eps, x3_0 + eps},
+                                       {x4_0 - eps, x4_0 + eps},
+                                       {x5_0 - eps, x5_0 + eps},
+                                       {x6_0 - eps, x6_0 + eps},
+                                       {x7_0 - eps, x7_0 + eps},
+                                       {t_0,        t_0}});
+
+        std::cout << "Initial set: " << initial_set << std::endl;
+        Real evolution_time(20.0);
+
+        StopWatch sw;
+
+        std::cout << "Computing orbit... " << std::flush;
+        auto orbit = evolver.orbit(evolver.enclosure(initial_set), evolution_time, Semantics::UPPER);
+        std::cout << "done." << std::endl;
+
+        Nat ce = 0;
+        for (auto set : orbit.reach()) {
+            if (definitely(set.bounding_box()[3].upper().raw() >= 4.5)) {
+                std::cout << "set with value " << set.bounding_box()[3] << " does not respect the specification."
+                          << std::endl;
+                ++ce;
+            }
+        }
+
+        sw.click();
+        std::cout << "Number of counterexamples: " << ce << std::endl;
+        std::cout << "Done in " << sw.elapsed() << " seconds." << std::endl;
+
+        reach1.adjoin(orbit.reach());
+    }*/
+
+    {
+        MaximumError max_err = 2e-3;
+        TaylorSeriesIntegrator integrator(max_err, Order(3u));
+
+        VectorFieldEvolver evolver(dynamics, integrator);
+        evolver.configuration().maximum_enclosure_radius(0.04);
+        evolver.configuration().maximum_step_size(0.2);
+        evolver.configuration().maximum_spacial_error(1e-3);
+        evolver.verbosity = 1;
+        std::cout << evolver.configuration() << std::endl;
+
+        Real eps = 1 / 20_q;
+
+        Box<RealInterval> initial_set({{x1_0 - eps, x1_0 + eps},
+                                       {x2_0 - eps, x2_0 + eps},
+                                       {x3_0 - eps, x3_0 + eps},
+                                       {x4_0 - eps, x4_0 + eps},
+                                       {x5_0 - eps, x5_0 + eps},
+                                       {x6_0 - eps, x6_0 + eps},
+                                       {x7_0 - eps, x7_0 + eps},
+                                       {t_0,        t_0}});
+
+        std::cout << "Initial set: " << initial_set << std::endl;
+        Real evolution_time(20.0);
+
+        StopWatch sw;
+
+        std::cout << "Computing orbit... " << std::flush;
+        auto orbit = evolver.orbit(evolver.enclosure(initial_set), evolution_time, Semantics::UPPER);
+        std::cout << "done." << std::endl;
+
+        Nat ce = 0;
+        for (auto set : orbit.reach()) {
+            if (definitely(set.bounding_box()[3].upper().raw() >= 4.5)) {
+                std::cout << "set with value " << set.bounding_box()[3] << " does not respect the specification."
+                          << std::endl;
+                ++ce;
+            }
+        }
+
+        sw.click();
+        std::cout << "Number of counterexamples: " << ce << std::endl;
+        std::cout << "Done in " << sw.elapsed() << " seconds." << std::endl;
+
+        reach2.adjoin(orbit.reach());
     }
 
-    plot("laub-loomis",PlanarProjectionMap(8,7,3),ApproximateBoxType({{1.5,5.0},{1.5,5.0},{1.5,5.0},{1.5,5.0},{1.5,5.0},{1.5,5.0},{1.5,5.0},{0.0,20.0}}), Colour(1.0,0.75,0.5), orbit);
+    std::cout << "plotting..." << std::endl;
+    Box<FloatDPUpperInterval> graphics_box{{1.5,5.0},{1.5,5.0},{1.5,5.0},{1.5,5.0},{1.5,5.0},{1.5,5.0},{1.5,5.0},{0.0,20.0}};
+    Figure fig=Figure();
+    fig.set_projection_map(PlanarProjectionMap(8,7,3));
+    fig.set_bounding_box(graphics_box);
+    fig.set_line_colour(0.0,0.0,0.0);
+    fig.set_line_style(false);
+    fig.set_fill_colour(0.6,0.6,0.6);
+    fig.draw(reach2);
+    fig.set_fill_colour(1.0,0.75,0.5);
+    //fig.draw(reach1);
+    fig.write("laub-loomis");
 }

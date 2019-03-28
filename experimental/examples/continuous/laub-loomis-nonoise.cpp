eecca5c7c0f8bee7395484fbe7ffdef33a01b1bf
Luca Geretti <luca.geretti@gmail.com>
2019-03-28 11:12:17 +0100

    Finalise Laub-Loomis example for ARCH

--- experimental/examples/continuous/laub-loomis-nonoise.cpp
+++ experimental/examples/continuous/laub-loomis-nonoise.cpp
@@ -52,7 +52,7 @@ int main() {
     Real t_0(0.0);
 
     ListSet<Enclosure> reach1, reach2, reach3;
-/*
+
     {
         MaximumError max_err = 2e-3;
         TaylorSeriesIntegrator integrator(max_err, Order(3u));
@@ -61,7 +61,7 @@ int main() {
         evolver.configuration().maximum_enclosure_radius(0.09);
         evolver.configuration().maximum_step_size(0.2);
         evolver.configuration().maximum_spacial_error(1e-3);
-        evolver.verbosity = 1;
+        evolver.verbosity = 0;
         std::cout << evolver.configuration() << std::endl;
 
         Real eps = 1 / 100_q;
@@ -98,7 +98,7 @@ int main() {
         std::cout << "Done in " << sw.elapsed() << " seconds." << std::endl;
 
         reach1.adjoin(orbit.reach());
-    }*/
+    }
 
     {
         MaximumError max_err = 2e-3;
@@ -108,7 +108,7 @@ int main() {
         evolver.configuration().maximum_enclosure_radius(0.04);
         evolver.configuration().maximum_step_size(0.2);
         evolver.configuration().maximum_spacial_error(1e-3);
-        evolver.verbosity = 1;
+        evolver.verbosity = 0;
         std::cout << evolver.configuration() << std::endl;
 
         Real eps = 1 / 20_q;
@@ -147,6 +147,53 @@ int main() {
         reach2.adjoin(orbit.reach());
     }
 
+    {
+        MaximumError max_err = 2e-3;
+        TaylorSeriesIntegrator integrator(max_err, Order(4u));
+
+        VectorFieldEvolver evolver(dynamics, integrator);
+        evolver.configuration().maximum_enclosure_radius(0.09);
+        evolver.configuration().maximum_step_size(0.2);
+        evolver.configuration().maximum_spacial_error(1e-3);
+        evolver.verbosity = 0;
+        std::cout << evolver.configuration() << std::endl;
+
+        Real eps = 1 / 10_q;
+
+        Box<RealInterval> initial_set({{x1_0 - eps, x1_0 + eps},
+                                       {x2_0 - eps, x2_0 + eps},
+                                       {x3_0 - eps, x3_0 + eps},
+                                       {x4_0 - eps, x4_0 + eps},
+                                       {x5_0 - eps, x5_0 + eps},
+                                       {x6_0 - eps, x6_0 + eps},
+                                       {x7_0 - eps, x7_0 + eps},
+                                       {t_0,        t_0}});
+
+        std::cout << "Initial set: " << initial_set << std::endl;
+        Real evolution_time(20.0);
+
+        StopWatch sw;
+
+        std::cout << "Computing orbit... " << std::flush;
+        auto orbit = evolver.orbit(evolver.enclosure(initial_set), evolution_time, Semantics::UPPER);
+        std::cout << "done." << std::endl;
+
+        Nat ce = 0;
+        for (auto set : orbit.reach()) {
+            if (definitely(set.bounding_box()[3].upper().raw() >= 4.5)) {
+                std::cout << "set with value " << set.bounding_box()[3] << " does not respect the specification."
+                          << std::endl;
+                ++ce;
+            }
+        }
+
+        sw.click();
+        std::cout << "Number of counterexamples: " << ce << std::endl;
+        std::cout << "Done in " << sw.elapsed() << " seconds." << std::endl;
+
+        reach3.adjoin(orbit.reach());
+    }
+
     std::cout << "plotting..." << std::endl;
     Box<FloatDPUpperInterval> graphics_box{{1.5,5.0},{1.5,5.0},{1.5,5.0},{1.5,5.0},{1.5,5.0},{1.5,5.0},{1.5,5.0},{0.0,20.0}};
     Figure fig=Figure();
@@ -154,9 +201,11 @@ int main() {
     fig.set_bounding_box(graphics_box);
     fig.set_line_colour(0.0,0.0,0.0);
     fig.set_line_style(false);
+    fig.set_fill_colour(1.0,0.75,0.5);
+    fig.draw(reach3);
     fig.set_fill_colour(0.6,0.6,0.6);
     fig.draw(reach2);
-    fig.set_fill_colour(1.0,0.75,0.5);
-    //fig.draw(reach1);
+    fig.set_fill_colour(1.0,1.0,1.0);
+    fig.draw(reach1);
     fig.write("laub-loomis");
 }

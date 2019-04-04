26d6d76cdb895b73767eb8f5b71375fab4d2dfd0
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2019-04-04 21:43:44 +0200

    Added custom LaTeX macros for MathJax support. Changed \jot to 1px.

---
+++ doc/macros.js
@@ -0,0 +1,26 @@
+MathJax.Hub.Config({
+  TeX: {
+    Macros: {
+//      jot: "1ex",
+
+      N: "{\\mathbb{R}}",
+      Z: "{\\mathbb{Z}}",
+      Q: "{\\mathbb{Q}}",
+      R: "{\\mathbb{R}}",
+      IF: "{\\mathbb{I\!F}}",
+      seq: ["{\\vec{#1}}",1],
+      fto: "{\\longrightarrow}",
+      interval: ["{[#1]}",1],
+      ivl: ["{[#1]}",1],
+      dt: ["{\\dot{#1}}",1],
+      unl: ["{\\underline{#1}}",1],
+      ovl: ["{\\overline{#1}}",1],
+      der: ["{\\dot{#1}}",1],
+      dag: "{\\dagger}",
+
+      cline: ["",1],
+      textsc: ["{\\mathsc{#1}}",1],
+      mathsc: ["\\text{#1}",1],
+    }
+  }
+});

959a068b1286d70104ad3f8291948fed4ed690c6
Pieter Collins <pieter.collins@maastrichtuniversity.nl>
2020-04-14 16:00:35 +0200

    Changed Doxygen comments in array.hpp to use one-line //! format.

--- source/utility/array.hpp
+++ source/utility/array.hpp
@@ -69,136 +69,136 @@ class Array {
     typedef std::ptrdiff_t difference_type;
   public:
 
-    /*! \brief Destructor */
+    //! \brief Destructor
     ~Array() { this->_destroy_elements(); uninitialized_delete(_ptr); }
 
-    /*! \brief Default constructor. Constructs an empty Array. */
+    //! \brief Default constructor. Constructs an empty Array.
     Array() : _size(0), _ptr(0) { }
-    /*! \brief Constructs an Array of size \a n with default-initialised elements. */
+    //! \brief Constructs an Array of size \a n with default-initialised elements.
     explicit Array(const SizeType n) : _size(n), _ptr(uninitialized_new(n)) { for(SizeType i=0; i!=n; ++i) { new (_ptr+i) T(); } }
-    /*! \brief Constructs an Array of size \a n with uninitialised elements. The elements should be initialised using placement new. */
+    //! \brief Constructs an Array of size \a n with uninitialised elements. The elements should be initialised using placement new.
     explicit Array(const SizeType n, Uninitialised) : _size(n), _ptr(uninitialized_new(n)) { }
-    /*! \brief Constructs an Array of size \a n with elements initialised to \a x. */
+    //! \brief Constructs an Array of size \a n with elements initialised to \a x.
     Array(const SizeType n, const ValueType& x) : _size(n), _ptr(uninitialized_new(n)) { this->_uninitialized_fill(x); }
 
-    /*! \brief Converts an initializer list to an Array. */
+    //! \brief Converts an initializer list to an Array.
     Array(InitializerList<T> lst) : _size(lst.size()), _ptr(uninitialized_new(_size)) {
         this->_uninitialized_fill(lst.begin()); }
-    /*! \brief Constructs an Array from an initializer list of doubles and a precision parameter. */
+    //! \brief Constructs an Array from an initializer list of doubles and a precision parameter.
     template<class PR, EnableIf<IsConstructible<T,double,PR>> = dummy>
     Array(InitializerList<double> lst, PR pr) : _size(lst.size()), _ptr(uninitialized_new(_size)) {
         this->_uninitialized_fill(lst.begin(),pr); }
     template<class PR, EnableIf<IsConstructible<T,ExactDouble,PR>> = dummy>
     Array(InitializerList<ExactDouble> lst, PR pr) : _size(lst.size()), _ptr(uninitialized_new(_size)) {
         this->_uninitialized_fill(lst.begin(),pr); }
-    /*! \brief Generate from a function (object) \a g of type \a G mapping an index to a value. */
+    //! \brief Generate from a function (object) \a g of type \a G mapping an index to a value.
     template<class G, EnableIf<IsInvocableReturning<ValueType,G,SizeType>> = dummy>
     Array(SizeType n, G const& g) : _size(n), _ptr(uninitialized_new(_size)) {
         this->_uninitialized_generate(g); }
 
-    /*! \brief Constructs an Array from the range \a first to \a last. */
+    //! \brief Constructs an Array from the range \a first to \a last.
     template<class ForwardIterator>
     Array(ForwardIterator first, ForwardIterator last)
             : _size(static_cast<SizeType>(std::distance(first,last))), _ptr(uninitialized_new(_size)) {
         assert(std::distance(first,last) >= 0);
         this->_uninitialized_fill(first); }
 
-    /*! \brief Conversion constructor. */
+    //! \brief Conversion constructor.
     template<class TT, EnableIf<IsConvertible<TT,T>> = dummy>
     Array(const Array<TT>& a) : _size(a.size()), _ptr(uninitialized_new(_size)) {
         this->_uninitialized_fill(a.begin()); }
 
-    /*! \brief Explicit conversion constructor. */
+    //! \brief Explicit conversion constructor.
     template<class TT, EnableIf<IsConstructible<T,TT>> = dummy, DisableIf<IsConvertible<TT,T>> =dummy>
     explicit Array(const Array<TT>& a) : _size(a.size()), _ptr(uninitialized_new(_size)) {
         this->_uninitialized_fill(a.begin()); }
 
-    /*! \brief Explicit construction with properties parameter. */
+    //! \brief Explicit construction with properties parameter.
     template<class TT, class PR, EnableIf<IsConstructible<T,TT,PR>> = dummy>
     Array(const Array<TT>& a, PR pr) : _size(a.size()), _ptr(uninitialized_new(_size)) {
         this->_uninitialized_fill(a.begin(),pr); }
 
-    /*! \brief Copy constructor. */
+    //! \brief Copy constructor.
     Array(const Array<T>& a) : _size(a.size()), _ptr(uninitialized_new(_size)) {
         this->_uninitialized_fill(a.begin()); }
-    /*! \brief Move constructor. */
+    //! \brief Move constructor.
     Array(Array<T>&& a) : _size(a._size), _ptr(a._ptr) {
         a._size=0u; a._ptr=nullptr; }
-    /*! \brief Copy assignment. */
+    //! \brief Copy assignment.
     Array<T>& operator=(const Array<T>& a) {
         if(this->size()==a.size()) { fill(a.begin()); }
         else { this->_destroy_elements(); uninitialized_delete(_ptr); _size=a.size(); _ptr=uninitialized_new(_size); this->_uninitialized_fill(a.begin()); }
         return *this; }
-    /*! \brief Move assignment. */
+    //! \brief Move assignment.
     Array<T>& operator=(Array<T>&& a) {
         if(this!=&a) { this->_size=a._size; this->_ptr=a._ptr; a._size=0u; a._ptr=nullptr; } return *this; }
 
-    /*! \brief True if the Array's size is 0. */
+    //! \brief True if the Array's size is 0.
     bool empty() const { return _size==0u; }
-    /*! \brief The size of the Array. */
+    //! \brief The size of the Array.
     SizeType size() const { return _size; }
-    /*! \brief The maximum possible size of the Array. */
+    //! \brief The maximum possible size of the Array.
     SizeType max_size() const { return (SizeType) (-1); }
-    /*! \brief Resizes the Array to hold \a n elements. If \a n is larger than the current size, the extra elements are default initialised. */
+    //! \brief Resizes the Array to hold \a n elements. If \a n is larger than the current size, the extra elements are default initialised.
     void resize(SizeType n) {
         if(size()!=n) {
             pointer _new_ptr=uninitialized_new(n);
             for(SizeType i=0; i!=n; ++i) { if(i<_size) { new (_new_ptr+i) T(_ptr[i]); } else { new (_new_ptr+i) T(); } }
             this->_destroy_elements(); uninitialized_delete(_ptr); _size=n; _ptr=_new_ptr; } }
-    /*! \brief Resizes the Array to hold \a n elements. If \a n is larger than the current size, the extra elements are initialised with value \a t. */
+    //! \brief Resizes the Array to hold \a n elements. If \a n is larger than the current size, the extra elements are initialised with value \a t.
     void resize(SizeType n, const T& t) {
         if(size()!=n) {
             pointer _new_ptr=uninitialized_new(n);
             for(SizeType i=0; i!=n; ++i) { if(i<_size) { new (_new_ptr+i) T(_ptr[i]); } else { new (_new_ptr+i) T(t); } }
             this->_destroy_elements(); uninitialized_delete(_ptr); _size=n; _ptr=_new_ptr; } }
-    /*! \brief Reallocates the Array to hold \a n elements. The new elements are default-constructed. */
+    //! \brief Reallocates the Array to hold \a n elements. The new elements are default-constructed.
     void reallocate(SizeType n) { if(size()!=n) { this->_destroy_elements(); uninitialized_delete(_ptr);
         _size=n; _ptr=uninitialized_new(_size); for(SizeType i=0; i!=_size; ++i) { new (_ptr+i) T(); } } }
-    /*! \brief Efficiently swap two arrays.  */
+    //! \brief Efficiently swap two arrays.
     void swap(Array<T>& a) { std::swap(_size,a._size); std::swap(_ptr,a._ptr); }
 
-    /*! \brief The \a n th element. */
+    //! \brief The \a n th element.
     ValueType& operator[](SizeType i) { return _ptr[i]; }
-    /*! \brief The \a n th element. */
+    //! \brief The \a n th element.
     const ValueType& operator[](SizeType i) const { return _ptr[i]; }
-    /*! \brief Checked access to the \a n th element. */
+    //! \brief Checked access to the \a n th element.
     ValueType& at(SizeType i) { if(i<_size) { return _ptr[i]; } else { throw std::out_of_range("Array: index out-of-range"); } }
-    /*! \brief Checked access to the \a n th element. */
+    //! \brief Checked access to the \a n th element.
     const ValueType& at(SizeType i) const { if(i<_size) { return _ptr[i]; } else { throw std::out_of_range("Array: index out-of-range"); } }
 
-    /*! \brief A reference to the first element of the Array. */
+    //! \brief A reference to the first element of the Array.
     ValueType& front() { return _ptr[0]; }
-    /*! \brief A constant reference to the first element of the Array. */
+    //! \brief A constant reference to the first element of the Array.
     const ValueType& front() const { return _ptr[0]; }
-    /*! \brief A reference to the last element of the Array. */
+    //! \brief A reference to the last element of the Array.
     ValueType& back() { return _ptr[_size-1]; }
-    /*! \brief A constant reference  to the last element of the Array. */
+    //! \brief A constant reference  to the last element of the Array.
     const ValueType& back() const { return _ptr[_size-1]; }
 
-    /*! \brief An Iterator pointing to the beginning of the Array. */
+    //! \brief An Iterator pointing to the beginning of the Array.
     Iterator begin() { return _ptr; }
-    /*! \brief A constant Iterator pointing to the beginning of the Array. */
+    //! \brief A constant Iterator pointing to the beginning of the Array.
     ConstIterator begin() const { return _ptr; }
-    /*! \brief An Iterator pointing to the end of the Array. */
+    //! \brief An Iterator pointing to the end of the Array.
     Iterator end() { return _ptr+_size; }
-    /*! \brief A constant Iterator pointing to the end of the Array. */
+    //! \brief A constant Iterator pointing to the end of the Array.
     ConstIterator end() const { return _ptr+_size; }
 
-    /*! \brief Tests two arrays for equality */
+    //! \brief Tests two arrays for equality
     bool operator==(const Array& other) const {
         if(size()!=other.size()) return false;
         T const* first=begin(); T const* last=end(); T const* curr=other.begin();
         while(first!=last) { if((*first)!=(*curr)) { return false; } ++first; ++curr; } return true; }
-    /*! \brief Tests two arrays for inequality */
+    //! \brief Tests two arrays for inequality
     bool operator!=(const Array& other) const { return !((*this)==other); }
 
-    /*! \brief Fills the Array with copies of \a x. */
+    //! \brief Fills the Array with copies of \a x.
     void fill(const ValueType& x) {
         ValueType* curr=begin(); ValueType* end=this->end(); while(curr!=end) { *curr=x; ++curr; } }
-    /*! \brief Fills the Array from the sequence starting at \a first. */
+    //! \brief Fills the Array from the sequence starting at \a first.
     template<class InputIterator> void fill(InputIterator first) {
         ValueType* curr=begin(); ValueType* end=this->end(); while(curr!=end) { *curr=*first; ++curr; ++first; } }
-    /*! \brief Assigns the sequence from \a first to \a last. */
+    //! \brief Assigns the sequence from \a first to \a last.
     template<class ForwardIterator> void assign(ForwardIterator first, ForwardIterator last) {
         resize(std::distance(first,last)); fill(first); }
   private:

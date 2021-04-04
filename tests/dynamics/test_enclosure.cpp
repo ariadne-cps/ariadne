b8ac6f8ada61b543047917a7907b5526479d4447
Luca Geretti <luca.geretti@gmail.com>
2021-04-04 16:27:31 +0200

    Test drawing on LabelledEnclosure

--- tests/dynamics/test_enclosure.cpp
+++ tests/dynamics/test_enclosure.cpp
@@ -61,10 +61,10 @@ class TestEnclosure
         ARIADNE_TEST_CALL(test_restriction());
         ARIADNE_TEST_CALL(test_auxiliary_map());
         ARIADNE_TEST_CALL(test_constraints());
-        ARIADNE_TEST_CALL(test_product());
-        ARIADNE_TEST_CALL(test_split());
-        ARIADNE_TEST_CALL(test_draw());
-        ARIADNE_TEST_CALL(test_labelled());
+        ARIADNE_TEST_CALL(test_labelled_construction());
+        //ARIADNE_TEST_CALL(test_labelled_product());
+        ARIADNE_TEST_CALL(test_labelled_split());
+        ARIADNE_TEST_CALL(test_labelled_draw());
     }
 
     Void test_construct_without_domain() const {
@@ -147,44 +147,7 @@ class TestEnclosure
         ARIADNE_TEST_ASSERT(is_indeterminate(encl.satisfies(-sqr(x0))));
     }
 
-    Void test_product() const {
-        ExactBoxType dom1({{0.0_x,2.0_x},{1.0_x,3.0_x}});
-        auto swp = ThresholdSweeper<FloatDP>(dp,1e-8);
-        auto fnc = antiderivative(ValidatedVectorMultivariateTaylorFunctionModelDP::identity(dom1,swp),0);
-        Enclosure encl1(dom1,fnc,EnclosureConfiguration(TaylorFunctionFactory(swp)));
-        ARIADNE_TEST_PRINT(product(encl1,ExactIntervalType(-1.0_x,1.0_x)));
-        ExactBoxType dom2({{1.0_x,5.0_x}});
-        ARIADNE_TEST_PRINT(product(encl1,dom2));
-        Enclosure encl2(dom2,EnclosureConfiguration(TaylorFunctionFactory(swp)));
-        ARIADNE_TEST_PRINT(product(encl1,encl2));
-    }
-
-    Void test_split() const {
-
-    }
-
-    Void _draw(Drawer const& drawer, String suffix) const {
-        ARIADNE_PRINT_TEST_COMMENT("Drawing with " + suffix + " method");
-        ARIADNE_TEST_PRINT(drawer);
-        ExactBoxType dom({{0.0_x,1.0_x},{1.0_x,2.0_x}});
-        auto swp = ThresholdSweeper<FloatDP>(dp,1e-8);
-        auto fnc = antiderivative(ValidatedVectorMultivariateTaylorFunctionModelDP::identity(dom,swp),0);
-        TaylorFunctionFactory function_factory(swp);
-        EnclosureConfiguration configuration(function_factory);
-        configuration.set_drawer(drawer);
-        Enclosure encl(dom,fnc,configuration);
-        Figure fig(dom,Projection2d(2,0,1));;
-        fig << line_style(true) << encl;
-        fig.write(("test_enclosure_"+suffix).c_str());
-    }
-
-    Void test_draw() const {
-        _draw(BoxDrawer(),"box");
-        _draw(GridDrawer(4),"grid");
-        _draw(AffineDrawer(1),"affine");
-    }
-
-    Void test_labelled() const {
+    Void test_labelled_construction() const {
         RealVariable x("x"), y("y"), z("z");
         RealSpace spc({x, y});
         ExactBoxType bx({{0.0_x,2.0_x},{1.0_x,3.0_x}});
@@ -205,6 +168,50 @@ class TestEnclosure
         encl3.set_auxiliary_mapping(auxiliary);
         LabelledEnclosure labelled_encl3(encl3,spc,RealSpace({z}));
         ARIADNE_TEST_PRINT(labelled_encl3);
+        ARIADNE_TEST_PRINT(labelled_encl3.state_auxiliary_space());
+    }
+
+    Void test_labelled_product() const {
+        RealVariable x("x"),y("y"),z("z");
+        ExactBoxType dom1({{0.0_x,2.0_x},{1.0_x,3.0_x}});
+        auto swp = ThresholdSweeper<FloatDP>(dp,1e-8);
+        auto fnc = antiderivative(ValidatedVectorMultivariateTaylorFunctionModelDP::identity(dom1,swp),0);
+        Enclosure e1(dom1,fnc,EnclosureConfiguration(TaylorFunctionFactory(swp)));
+        LabelledEnclosure le1(e1,RealSpace({x,y}),RealSpace({z}));
+        ARIADNE_TEST_PRINT(product(le1,LabelledExactIntervalType(z,ExactIntervalType(-1.0_x,1.0_x))));
+        ExactBoxType dom2({{-1.0_x,1.0_x}});
+        LabelledExactBoxType box2(RealSpace({z}),dom2);
+        ARIADNE_TEST_PRINT(product(le1,box2));
+        Enclosure e2(dom2,EnclosureConfiguration(TaylorFunctionFactory(swp)));
+        LabelledEnclosure le2(e2,RealSpace({x,y}),RealSpace({z}));
+        ARIADNE_TEST_PRINT(product(le1,le2));
+    }
+
+    Void test_labelled_split() const {
+
+    }
+
+    Void _draw(Drawer const& drawer, String suffix) const {
+        ARIADNE_PRINT_TEST_COMMENT("Drawing with " + suffix + " method");
+        ARIADNE_TEST_PRINT(drawer);
+        ExactBoxType dom({{0.0_x,1.0_x},{1.0_x,2.0_x}});
+        auto swp = ThresholdSweeper<FloatDP>(dp,1e-8);
+        auto fnc = antiderivative(ValidatedVectorMultivariateTaylorFunctionModelDP::identity(dom,swp),0);
+        TaylorFunctionFactory function_factory(swp);
+        EnclosureConfiguration configuration(function_factory);
+        configuration.set_drawer(drawer);
+        Enclosure encl(dom,fnc,configuration);
+        RealVariable x("x"),y("y");
+        LabelledEnclosure lencl(encl,RealSpace({x,y}));
+        LabelledFigure fig(Axes2d(-1.0<=x<=1.0,-1.5<=y<=1.5));;
+        fig << line_style(true) << lencl;
+        fig.write(("test_enclosure_"+suffix).c_str());
+    }
+
+    Void test_labelled_draw() const {
+        _draw(BoxDrawer(),"box");
+        _draw(GridDrawer(4),"grid");
+        _draw(AffineDrawer(1),"affine");
     }
 };
 

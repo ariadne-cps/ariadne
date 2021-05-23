f2763e4628392c86479c9bd23737456d2ab3f1aa
Luca Geretti <luca.geretti@gmail.com>
2021-05-23 19:28:06 +0200

    Add Python bindings for Logger selection of scheduler

---
+++ python/source/io_submodule.cpp
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *            io_submodule.cpp
+ *
+ *  Copyright  2008-21  Luca Geretti
+ *
+ ****************************************************************************/
+
+/*
+ *  This file is part of Ariadne.
+ *
+ *  Ariadne is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Ariadne is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Ariadne.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "pybind11.hpp"
+#include "utilities.hpp"
+
+#include "io/logging.hpp"
+
+using namespace Ariadne;
+
+Void export_logger(pybind11::module& module)
+{
+    auto const& reference = pybind11::return_value_policy::reference;
+
+    pybind11::class_<Logger> logger_class(module,"Logger");
+    logger_class.def_static("instance", &Logger::instance, reference);
+    logger_class.def("use_immediate_scheduler", &Logger::use_immediate_scheduler);
+    logger_class.def("use_blocking_scheduler", &Logger::use_blocking_scheduler);
+    logger_class.def("use_nonblocking_scheduler", &Logger::use_nonblocking_scheduler);
+}
+
+Void io_submodule(pybind11::module& module)
+{
+    export_logger(module);
+}
+

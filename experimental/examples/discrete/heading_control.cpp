79adbf65b2787afab1bf5a0d8a8b1e8e272f9082
Luca Geretti <luca.geretti@gmail.com>
2023-02-01 19:36:43 -0800

    Create initial example for discrete time heading control

---
+++ experimental/examples/discrete/heading_control.cpp
@@ -0,0 +1,64 @@
+/***************************************************************************
+ *            heading_control.cpp
+ *
+ *  Copyright  2023  Luca Geretti
+ *
+ ****************************************************************************/
+
+/*
+ *  This file is part of Ariadne.
+ *
+ *  Ariadne is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Ariadne is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Ariadne.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "ariadne_main.hpp"
+#include "utility/stopwatch.hpp"
+
+void ariadne_main()
+{
+    Real deltat=0.1_dec;
+    Real v=0.5_dec;
+    RealVariable x("x"), y("y"), theta("theta"), Kx("Kx"), Ky("Ky"), Kt("Kt"), b("b");
+    IteratedMap heading({next(x)=x+deltat*v*cos(theta),next(y)=y+deltat*v*sin(theta),next(theta)=theta+deltat*(Kx*x+Ky*y+Kt*theta+b),
+                         next(Kx)=Kx,next(Ky)=Ky,next(Kt)=Kt,next(b)=b});
+    CONCLOG_PRINTLN_VAR(heading);
+
+    // Set up the evaluators
+    IteratedMapEvolver evolver(heading);
+
+    Real d = 0.5_dec;
+    // Set-up initial set and time for evolution
+    RealExpressionBoundedConstraintSet initial_set={0.0_dec<=x<=0.5_dec,0.0_dec<=y<=0.5_dec,0.0_dec<=theta<=0.3926_dec,-d<=Kx<=d,-d<=Ky<=d,-d<=Kt<=d,-d<=b<=d};
+
+    // Set up the evolution parameters and grid
+    Integer evolve_time(1);
+
+    Stopwatch<Milliseconds> sw;
+    CONCLOG_PRINTLN("Computing evolution...");
+    SizeType num_iterations = 552*160;
+
+    auto function = heading.function();
+    List<UpperBoxType> boxes;
+    auto initial_box =initial_set.euclidean_set(heading.state_space()).bounding_box();
+    for (SizeType i=0; i<num_iterations; ++i) {
+        boxes.push_back(apply(function,initial_box));
+    }
+    sw.click();
+    CONCLOG_PRINTLN_AT(1,"Using Function representation of the map: " << sw.elapsed_seconds()/num_iterations << " seconds on average.");
+
+/*
+    LabelledFigure fig(Axes2d(0<=x<=20,0<=y<=20));
+    fig << orbit;
+    fig.write("heading_control");*/
+}

7e6ce22d9aab7e4d3c3297f34ec53030568b0d17
Luca Geretti <luca.geretti@gmail.com>
2023-02-18 12:27:10 -0800

    Refactor code for different workspaces

--- experimental/examples/discrete/heading_control.cpp
+++ experimental/examples/discrete/heading_control.cpp
@@ -51,6 +51,49 @@ Tuple<IteratedMap,Grid,BoundsBoxType> cpwa_control() {
     return Tuple<IteratedMap,Grid,BoundsBoxType>(heading,control_grid,control_domain);
 }
 
+ReachAvoid set_workspace_1(EffectiveVectorMultivariateFunction const& dynamics, Grid const& control_grid, BoundsBoxType const& control_domain) {
+    double pi_ = pi.get_d();
+
+    Grid state_grid({0.5,0.5,2*pi/8});
+    BoundType theta_domain = {0,2*pi_};
+    BoundsBoxType state_domain({{0,5},{0,5},theta_domain});
+    SizeType depth = 0;
+
+    ReachAvoid ra("heading", dynamics, state_grid, state_domain, control_grid, control_domain, depth, 1e-10_x);
+
+    ra.add_obstacle({{1,3.5},{4.5,5},theta_domain});
+    ra.add_obstacle({{0,1},{2,3},theta_domain});
+    ra.add_obstacle({{2.5,5},{2,3},theta_domain});
+    ra.add_obstacle({{0,5},{0,0.5},theta_domain});
+
+    ra.add_goal({{4,5},{4.5,5},theta_domain});
+
+    return ra;
+}
+
+ReachAvoid set_workspace_2(EffectiveVectorMultivariateFunction const& dynamics, Grid const& control_grid, BoundsBoxType const& control_domain) {
+
+    double pi_ = pi.get_d();
+
+    Grid state_grid({0.25,0.25,2*pi/8});
+    BoundType theta_domain = {0,2*pi_};
+    BoundsBoxType state_domain({{0,5},{0,5},theta_domain});
+    SizeType depth = 0;
+
+    ReachAvoid ra("heading", dynamics, state_grid, state_domain, control_grid, control_domain, depth, 1e-10_x);
+
+    ra.add_obstacle({{0,0.5},{0,5},theta_domain});
+    ra.add_obstacle({{1.5,2},{0,3},theta_domain});
+    ra.add_obstacle({{1.5,2},{4,5},theta_domain});
+    ra.add_obstacle({{3,3.5},{0,1},theta_domain});
+    ra.add_obstacle({{3,3.5},{2,5},theta_domain});
+    ra.add_obstacle({{4.5,5},{0,5},theta_domain});
+
+    ra.add_goal({{3.75,4.25},{4.5,5},theta_domain});
+
+    return ra;
+}
+
 void ariadne_main()
 {
     auto sys = u_control();
@@ -60,63 +103,50 @@ void ariadne_main()
         dynamics[i] = get<0>(sys).function().get(i);
     CONCLOG_PRINTLN_VAR(dynamics);
 
-    double pi_ = pi.get_d();
+    auto ra = set_workspace_1(dynamics,get<1>(sys), get<2>(sys));
 
-    Grid state_grid({0.5,0.5,2*pi/8});
-    BoundType theta_domain = {0,2*pi_};
-    BoundsBoxType state_domain({{0,5},{0,5},theta_domain});
-    SizeType depth = 0;
+    CONCLOG_PRINTLN_VAR_AT(1,ra.state_size())
+    CONCLOG_PRINTLN_VAR_AT(1,ra.controller_size())
 
-    ReachAvoid scs("heading", dynamics, state_grid, state_domain, get<1>(sys), get<2>(sys), depth, 1e-10_x);
+    CONCLOG_RUN_AT(2,ra.print_obstacles())
+    CONCLOG_PRINTLN_VAR_AT(1,ra.obstacles_size())
 
-    CONCLOG_PRINTLN_VAR_AT(1,scs.state_size())
-    CONCLOG_PRINTLN_VAR_AT(1,scs.controller_size())
+    CONCLOG_RUN_AT(2,ra.print_goals())
+    CONCLOG_PRINTLN_VAR_AT(1,ra.goals_size())
 
-    scs.add_obstacle({{1,3.5},{4.5,5},theta_domain});
-    scs.add_obstacle({{0,1},{2,3},theta_domain});
-    scs.add_obstacle({{2.5,5},{2,3},theta_domain});
-    scs.add_obstacle({{0,5},{0,0.5},theta_domain});
-    CONCLOG_RUN_AT(2,scs.print_obstacles())
-    CONCLOG_PRINTLN_VAR_AT(1,scs.obstacles_size())
-
-    scs.add_goal({{4,5},{4.5,5},theta_domain});
-
-    CONCLOG_RUN_AT(2,scs.print_goals())
-
-    CONCLOG_PRINTLN_VAR_AT(1,scs.goals_size())
-    CONCLOG_PRINTLN_VAR_AT(1,scs.unverified_size())
+    CONCLOG_PRINTLN_VAR_AT(1,ra.unverified_size())
 
     Stopwatch<Milliseconds> sw;
-    scs.compute_reachability_graph();
+    ra.compute_reachability_graph();
     sw.click();
     CONCLOG_PRINTLN_AT(1,"Time cost of constructing reachability graph: " << sw.elapsed_seconds() << " seconds")
 
-    CONCLOG_PRINTLN_VAR_AT(1,scs.num_transitions())
+    CONCLOG_PRINTLN_VAR_AT(1,ra.num_transitions())
 
     sw.restart();
-    scs.refine_to_safety_graph();
+    ra.refine_to_safety_graph();
     sw.click();
     CONCLOG_PRINTLN_AT(1,"Time cost of reducing graph to safe one: " << sw.elapsed_seconds() << " seconds")
 
-    CONCLOG_PRINTLN_VAR_AT(1,scs.num_transitions())
+    CONCLOG_PRINTLN_VAR_AT(1,ra.num_transitions())
 
-    CONCLOG_PRINTLN_AT(1,"Safe abstract states: " << scs.num_sources())
+    CONCLOG_PRINTLN_AT(1,"Safe abstract states: " << ra.num_sources())
 
     sw.restart();
-    scs.refine_to_goal_reachable_graph();
+    ra.refine_to_goal_reachable_graph();
     sw.click();
     CONCLOG_PRINTLN_AT(1,"Time cost of reducing graph to goal-reachable one: " << sw.elapsed_seconds() << " seconds")
 
-    CONCLOG_PRINTLN_VAR_AT(1,scs.num_transitions())
+    CONCLOG_PRINTLN_VAR_AT(1,ra.num_transitions())
 
-    CONCLOG_PRINTLN_AT(1,"Safe goal-reachable abstract states: " << scs.num_sources())
+    CONCLOG_PRINTLN_AT(1,"Safe goal-reachable abstract states: " << ra.num_sources())
 
-    scs.update_unverified();
-    CONCLOG_PRINTLN_AT(1,"Unverified abstract states: " << scs.unverified_size() << " (" << scs.unverified_percentage() << "% left)")
+    ra.update_unverified();
+    CONCLOG_PRINTLN_AT(1,"Unverified abstract states: " << ra.unverified_size() << " (" << ra.unverified_percentage() << "% left)")
 
-    scs.plot({{0,5},{0,5},{-6.28_x,6.28_x}},0,1);
-    scs.plot({{0,5},{0,5},{-6.28_x,6.28_x}},0,2);
-    scs.plot({{0,5},{0,5},{-6.28_x,6.28_x}},1,2);
+    ra.plot({{0,5},{0,5},{-6.28_x,6.28_x}},0,1);
+    ra.plot({{0,5},{0,5},{-6.28_x,6.28_x}},0,2);
+    ra.plot({{0,5},{0,5},{-6.28_x,6.28_x}},1,2);
 
-    CONCLOG_RUN_AT(3,scs.print_graph())
+    CONCLOG_RUN_AT(3,ra.print_graph())
 }

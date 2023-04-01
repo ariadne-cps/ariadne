b44bbdb54e839ef8ded1b44787a80ca432532eb3
Luca Geretti <luca.geretti@gmail.com>
2023-03-31 22:19:36 -0700

    Add Brusselator experimental example

--- experimental/examples/continuous/brusselator.cpp
+++ experimental/examples/continuous/brusselator.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************
- *            laub-loomis.cpp
+ *            brusselator.cpp
  *
- *  Copyright  2018  Luca Geretti
+ *  Copyright  2023  Luca Geretti
  *
  ****************************************************************************/
 
@@ -30,18 +30,12 @@ using namespace Ariadne;
 void ariadne_main()
 {
     RealVariable x1("x1"),x2("x2"),x3("x3"),x4("x4"),x5("x5"),x6("x6"),x7("x7");
-
-    VectorField dynamics({dot(x1)=1.4_dec*x3-0.9_dec*x1,
-                          dot(x2)=2.5_dec*x5-1.5_dec*x2,
-                          dot(x3)=0.6_dec*x7-0.8_dec*x2*x3,
-                          dot(x4)=2.0_dec-1.3_dec*x3*x4,
-                          dot(x5)=0.7_dec*x1-x4*x5,
-                          dot(x6)=0.3_dec*x1-3.1_dec*x6,
-                          dot(x7)=1.8_dec*x6-1.5_dec*x2*x7
-                         });
+    RealConstant a("a",1),b("b",1.5_dec);
+    VectorField dynamics({dot(x1)=a+sqr(x1)*x2-b*x1-x1, dot(x2)=b*x1-sqr(x1)*x2});
 
     StepMaximumError max_err=1e-6;
-    TaylorPicardIntegrator integrator(max_err);
+    //TaylorPicardIntegrator integrator(max_err);
+    GradedTaylorSeriesIntegrator integrator(max_err,ThresholdSweeper<FloatDP>(DoublePrecision(),1e-8),LipschitzTolerance(0.5_x),MaximumTemporalOrder(5));
 
     VectorFieldEvolver evolver(dynamics,integrator);
     evolver.configuration().set_maximum_enclosure_radius(1.0);
@@ -49,19 +43,10 @@ void ariadne_main()
     evolver.configuration().set_maximum_spacial_error(1e-6);
     CONCLOG_PRINTLN(evolver.configuration())
 
-    Real x1_0(1.2_dec);
-    Real x2_0(1.05_dec);
-    Real x3_0(1.5_dec);
-    Real x4_0(2.4_dec);
-    Real x5_0(1.0_dec);
-    Real x6_0(0.1_dec);
-    Real x7_0(0.45_dec);
-    Real eps = 1/10_q;
-
-    RealExpressionBoundedConstraintSet initial_set({{x1_0-eps<=x1<=x1_0+eps},{x2_0-eps<=x2<=x2_0+eps},{x3_0-eps<=x3<=x3_0+eps},{x4_0-eps<=x4<=x4_0+eps},{x5_0-eps<=x5<=x5_0+eps},{x6_0-eps<=x6<=x6_0+eps},{x7_0-eps<=x7<=x7_0+eps}});
+    RealExpressionBoundedConstraintSet initial_set({{0.8_dec<=x1<=1},{0<=x2<=0.2_dec}});
 
     CONCLOG_PRINTLN_VAR(initial_set);
-    Real evolution_time(10);
+    Real evolution_time(3);
 
     Stopwatch<Milliseconds> sw;
     CONCLOG_PRINTLN("Computing orbit...")
@@ -70,20 +55,10 @@ void ariadne_main()
     CONCLOG_PRINTLN("Done in " << sw.elapsed_seconds() << " seconds.");
 
     CONCLOG_PRINTLN("Plotting...")
-    TimeVariable t;
-    /*
-    for (auto const& v : dynamics.state_space().variables()) {
-        CONCLOG_PRINTLN_AT(1,"Plotting t-" << v.name())
-        LabelledFigure fig(Axes2d{{0<=t<=evolution_time},{0<=v<=5}});
-        fig << orbit;
-        char filename[64];
-        snprintf(filename,64,"laub-loomis_t-%s",v.name().c_str());
-        fig.write(filename);
-    }*/
-    CONCLOG_PRINTLN_AT(1,"Plotting x5-x7")
-    LabelledFigure fig(Axes2d{{0<=x5<=1.2_dec},{0.1_dec<=x7<=0.6_dec}});
+    CONCLOG_PRINTLN_AT(1,"Plotting...")
+    LabelledFigure fig(Axes2d{{0<=x1<=2},{0<=x2<=2}});
     fig << orbit;
-    fig.write("laub-loomis_x5-x7");
+    fig.write("brusselator");
     sw.click();
     CONCLOG_PRINTLN("Done in " << sw.elapsed_seconds() << " seconds.");
 }

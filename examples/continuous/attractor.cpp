ff171770f2e0472c0ba5ea84251b25eae681ab66
Luca Geretti <luca.geretti@gmail.com>
2023-04-25 13:58:22 -0700

    Clean up attractor and vanderpol from spurious ;

--- examples/continuous/attractor.cpp
+++ examples/continuous/attractor.cpp
@@ -31,14 +31,14 @@ void ariadne_main()
     VectorField system = {{dot(x)=2*x-x*y,dot(y)=2*x*x-y}};
     RealExpressionBoundedConstraintSet initial_set = {{0.9_dec<=x<=1,-2.2_dec<=y<=-2},{sqr(x)+sqr(y+2)<=1}};
     RealExpressionBoundedConstraintSet safe_set = {{-1<=x<=4,-4<=y<=6},{sqr(x-2)+sqr(y-1)<=22}};
-    CONCLOG_PRINTLN_VAR(system);
-    CONCLOG_PRINTLN_VAR(initial_set);
-    CONCLOG_PRINTLN_VAR(safe_set);
+    CONCLOG_PRINTLN_VAR(system)
+    CONCLOG_PRINTLN_VAR(initial_set)
+    CONCLOG_PRINTLN_VAR(safe_set)
 
     auto initial_constraint_set = initial_set.euclidean_set(system.state_space());
     auto safe_constraint_set = safe_set.euclidean_set(system.state_space());
-    CONCLOG_PRINTLN_VAR(initial_constraint_set);
-    CONCLOG_PRINTLN_VAR(safe_constraint_set);
+    CONCLOG_PRINTLN_VAR(initial_constraint_set)
+    CONCLOG_PRINTLN_VAR(safe_constraint_set)
 
     Real evolution_time = 50;
 
@@ -47,16 +47,16 @@ void ariadne_main()
     VectorFieldSimulator simulator(system);
     simulator.configuration().set_step_size(0.1);
     simulator.configuration().set_num_subdivisions(6);
-    CONCLOG_PRINTLN("Simulating...");
+    CONCLOG_PRINTLN("Simulating...")
     auto orbit = simulator.orbit(initial_set,evolution_time);
     sw.click();
-    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.");
+    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.")
     LabelledFigure g(Axes2d{{-2<=x<=5},{-4<=y<=6}});
     g << orbit;
     g.write("attractor_simulation");
 
     AffineIntegrator affine_integrator(2,1);
-    CONCLOG_PRINTLN("Evolving with affine integrator...");
+    CONCLOG_PRINTLN("Evolving with affine integrator...")
     Configuration<VectorFieldEvolver> configuration;
     configuration.set_maximum_step_size(0.1);
     configuration.set_maximum_spacial_error(1e-2);
@@ -64,18 +64,18 @@ void ariadne_main()
     sw.restart();
     auto affine_evolver_orbit = affine_evolver.orbit(initial_set,evolution_time,Semantics::UPPER);
     sw.click();
-    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.");
+    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.")
     g.clear();
     g << affine_evolver_orbit;
     g.write("attractor_evolution_affine");
 
     GradedTaylorSeriesIntegrator nonlinear_integrator(0.001);
-    CONCLOG_PRINTLN("Evolving with nonlinear integrator...");
+    CONCLOG_PRINTLN("Evolving with nonlinear integrator...")
     VectorFieldEvolver nonlinear_evolver(system,configuration,nonlinear_integrator);
     sw.restart();
     auto nonlinear_evolver_orbit = nonlinear_evolver.orbit(initial_set,evolution_time,Semantics::UPPER);
     sw.click();
-    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.");
+    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.")
     g.clear();
     g << nonlinear_evolver_orbit;
     g.write("attractor_evolution_nonlinear");
@@ -85,12 +85,12 @@ void ariadne_main()
     analyser.configuration().set_lock_to_grid_time(0.75_dec);
     analyser.configuration().set_maximum_grid_extent(5);
 
-    CONCLOG_PRINTLN("Computing safety...");
+    CONCLOG_PRINTLN("Computing safety...")
     sw.restart();
     auto safety = analyser.verify_safety(initial_constraint_set,safe_constraint_set);
     sw.click();
-    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.");
-    CONCLOG_PRINTLN_VAR(safety.is_safe);
+    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.")
+    CONCLOG_PRINTLN_VAR(safety.is_safe)
     g.clear();
     g << fill_colour(lightgrey) << safety.safe_set
       << fill_colour(orange) << safety.chain_reach_set;
--- examples/continuous/vanderpol.cpp
+++ examples/continuous/vanderpol.cpp
@@ -27,7 +27,7 @@
 
 void ariadne_main()
 {
-    CONCLOG_PRINTLN("van der Pol oscillator");
+    CONCLOG_PRINTLN("van der Pol oscillator")
 
     RealConstant mu("mu",1);
     RealVariable x("x"), y("y");
@@ -47,7 +47,7 @@ void ariadne_main()
         set_maximum_spacial_error(1e-6);
 
     VectorFieldEvolver evolver(dynamics,configuration,integrator);
-    CONCLOG_PRINTLN(evolver.configuration());
+    CONCLOG_PRINTLN(evolver.configuration())
 
     Real x0 = 1.40_dec;
     Real y0 = 2.40_dec;
@@ -56,27 +56,27 @@ void ariadne_main()
 
     RealExpressionBoundedConstraintSet initial_set({x0-eps_x0<=x<=x0+eps_x0,y0-eps_y0<=y<=y0+eps_y0});
 
-    CONCLOG_PRINTLN("Initial set: " << initial_set);
+    CONCLOG_PRINTLN("Initial set: " << initial_set)
     Real evolution_time = 7;
 
     Helper::Stopwatch<std::chrono::milliseconds> sw;
-    CONCLOG_PRINTLN("Computing simulation...");
+    CONCLOG_PRINTLN("Computing simulation...")
     auto simulation = simulator.orbit(initial_set,evolution_time);
     sw.click();
-    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.");
+    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.")
 
-    CONCLOG_PRINTLN("Plotting...");;
+    CONCLOG_PRINTLN("Plotting...")
     LabelledFigure fig=LabelledFigure({-2.5<=x<=2.5,-3<=y<=3});
     fig.draw(simulation);
     fig.write("vanderpol_simulation");
 
     sw.restart();
-    CONCLOG_PRINTLN("Computing evolution... ");
+    CONCLOG_PRINTLN("Computing evolution... ")
     auto evolution = evolver.orbit(initial_set,evolution_time,Semantics::UPPER);
     sw.click();
-    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.");
+    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.")
 
-    CONCLOG_PRINTLN("Plotting...");
+    CONCLOG_PRINTLN("Plotting...")
     fig.clear();
     fig.draw(evolution);
     fig.write("vanderpol_evolution");

e5c39877e14b226f3f82d3224ab2c784b20a85c0
Luca Geretti <luca.geretti@gmail.com>
2023-10-12 17:10:27 +0200

    Move to discrete dynamics with comparison across all states and controls

--- experimental/examples/discrete/probability_map.cpp
+++ experimental/examples/discrete/probability_map.cpp
@@ -26,16 +26,17 @@
 #include "utility/stopwatch.hpp"
 
 #include "reach_avoid.hpp"
-
 using namespace Ariadne;
 
 void ariadne_main()
 {
-    RealVariable x("x"),y("y"),theta("theta"),u("u");
-    RealConstant vel("vel",3);
+    Real deltat=0.1_dec, v=3;
+    RealVariable x("x"), y("y"), theta("theta"), u("u");
+    IteratedMap sys({next(x)=x+deltat*v*cos(theta),next(y)=y+deltat*v*sin(theta),next(theta)= theta+u,next(u)=u});
 
+    FloatDP eps(1e-8_x,DoublePrecision());
     double probability_threshold = 0.05;
-    SizeType point_accuracy = 4;
+    SizeType point_accuracy = 6;
 
     RealSpace state_spc({x,y,theta});
     RealSpace control_spc({u});
@@ -58,218 +59,194 @@ void ariadne_main()
     }
     Grid state_grid(real_state_grid_lengths);
 
-    GridTreePaving domain_paving(state_grid);
-    UpperBoxType state_domain({{0.0_dec,5_dec},{0.0_dec,5_dec}, {0.0_dec,2*pi}});
-    domain_paving.adjoin_outer_approximation(state_domain,0);
-    domain_paving.mince(0);
+    Vector<Real> real_control_grid_lengths(control_spc.dimension());
+    {
+        SizeType idx = 0;
+        for (auto const& v : control_spc.variables()) {
+            for (auto const& gl : control_grid_lengths) {
+                if (gl.first == v) {
+                    real_control_grid_lengths[idx] = gl.second;
+                    ++idx;
+                    break;
+                }
+            }
+        }
+    }
+    Grid control_grid(real_control_grid_lengths);
+
+    GridTreePaving state_paving(state_grid);
+    UpperBoxType state_domain({{0.0_dec+eps,5_dec-eps},{0.0_dec+eps,5_dec-eps}, {0.0_dec+eps,2*pi-eps}});
+    state_paving.adjoin_outer_approximation(state_domain,0);
+    state_paving.mince(0);
+
+    GridTreePaving control_paving(control_grid);
+    UpperBoxType control_domain({{0.0_dec+eps,2*pi-eps}});
+    control_paving.adjoin_outer_approximation(control_domain,0);
+    control_paving.mince(0);
 
     IdentifiedCellFactory::HashTableType state_cells_ids;
 
-    auto default_cell_extent = domain_paving.begin()->root_extent();
-    CONCLOG_PRINTLN_VAR(default_cell_extent);
-    CONCLOG_PRINTLN_VAR(domain_paving.size());
-    for (auto const& c : domain_paving) {
-        state_cells_ids.insert(make_pair(word_to_id(c.word(),(default_cell_extent)*domain_paving.dimension()),state_cells_ids.size()));
+    auto default_cell_extent = state_paving.begin()->root_extent();
+    CONCLOG_PRINTLN_VAR(default_cell_extent)
+    CONCLOG_PRINTLN_VAR(state_paving.size())
+    CONCLOG_PRINTLN_VAR(control_paving.size())
+    for (auto const& c : state_paving) {
+        state_cells_ids.insert(make_pair(word_to_id(c.word(),(default_cell_extent)*state_paving.dimension()),state_cells_ids.size()));
     }
 
     auto identified_cell_factory = IdentifiedCellFactory(default_cell_extent,state_cells_ids);
 
-    RealSpace full_spc = join(state_spc,control_spc);
+    ProgressIndicator indicator(state_paving.size()*control_paving.size());
+    for (auto const& state_cell : state_paving) {
+        for (auto const& control_cell : control_paving) {
+            auto combined_cell = product(state_cell.box(), control_cell.box());
 
-    List<DottedRealAssignment> assignments({dot(x)=vel*cos(theta),
-                                                   dot(y)=vel*sin(theta),
-                                                   dot(theta)=u,
-                                                   dot(u)=0
-                                           });
+            auto dynamics = sys.function().zeros(state_spc.size(),sys.dimension());
+            for (SizeType i=0; i<state_spc.size(); ++i)
+                dynamics[i] = sys.function().get(i);
 
-    List<DottedRealAssignment> inverted_assignments;
-    for (auto const& a : assignments) {
-        inverted_assignments.push_back(DottedRealAssignment(a.lhs,-a.rhs));
-    }
+            Stopwatch<Microseconds> sw;
 
-    RealVariablesBox initial({0.0_dec<=x<=0.5_dec,0.0_dec<=y<=0.5_dec,0.0_dec<=theta<=pi/4,0.0_dec<=u<=pi/4});
+            Map<SizeType,double> interval_probabilities;
 
-    StepMaximumError max_err=1e-6;
-    TaylorPicardIntegrator integrator(max_err);
+            CONCLOG_PRINTLN_AT(1,"Computing using Interval Arithmetic...")
 
-    VectorFieldEvolver evolver(assignments,integrator);
-    evolver.configuration().set_maximum_enclosure_radius(1.0);
-    evolver.configuration().set_maximum_step_size(0.025);
-    evolver.configuration().set_maximum_spacial_error(1e-4);
+            auto final_box = shrink(cast_exact_box(apply(dynamics, combined_cell).bounding_box()),eps);
 
-    CONCLOG_PRINTLN_VAR(initial);
-    Real evolution_time(0.1_dec);
+            GridTreePaving final_paving(state_grid);
+            final_paving.adjoin_outer_approximation(final_box,0);
+            final_paving.restrict(state_paving);
+            final_paving.mince(0);
 
-    Stopwatch<Milliseconds> sw;
+            if (not final_paving.is_empty()) {
 
-    Map<SizeType,double> interval_probabilities;
+                GridTreePaving paving = intersection(state_paving,final_paving);
 
-    {
-    CONCLOG_PRINTLN("Computing using Interval Arithmetic...")
-    auto interval_orbit = evolver.orbit(initial,evolution_time,Semantics::UPPER);
+                double total_volume = 0;
+                for (auto const& cell : paving) {
+                    auto icell = identified_cell_factory.create(cell);
+                    auto starting_box = intersection(cell.box(),final_box);
+                    auto current_volume = starting_box.volume().get_d();
+                    interval_probabilities.insert(icell.id(),current_volume);
+                    total_volume += current_volume;
+                }
 
-    auto final_box = interval_orbit.final().bounding_box();
+                double maximum_interval_probability = 0;
+                for (auto& p : interval_probabilities) {
+                    maximum_interval_probability = std::max(maximum_interval_probability,p.second);
+                }
 
-    UpperBoxType final_projected_box(3,UpperIntervalType::empty_interval());
-    {
-    SizeType idx = 0;
-    for (auto const& v : final_box.space().variables()) {
-        if (state_spc.contains(v)) {
-            final_projected_box[idx] = final_box[v];
-            ++idx;
-        }
-    }
-    }
+                Set<SizeType> to_remove;
+                double remaining_total_probability = 0;
+                for (auto& p : interval_probabilities) {
+                    p.second = p.second/total_volume;
+                    if (p.second < probability_threshold*maximum_interval_probability) to_remove.insert(p.first);
+                    else remaining_total_probability += p.second;
+                }
 
-    GridTreePaving final_paving(state_grid);
-    final_paving.adjoin_outer_approximation(final_projected_box,0);
+                interval_probabilities.remove_keys(to_remove);
+                CONCLOG_PRINTLN_AT(1,"Interval probabilities:")
+                for (auto& p : interval_probabilities) {
+                    p.second = p.second/remaining_total_probability;
+                    CONCLOG_PRINTLN_AT(1,p.first << ": " << p.second)
+                }
 
-    GridTreePaving paving = intersection(domain_paving,final_paving);
+                sw.click();
+                CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds()*1000 << " ms.")
 
-    double total_volume = 0;
-    for (auto const& cell : paving) {
-        auto icell = identified_cell_factory.create(cell);
-        auto starting_box = intersection(cell.box(),final_projected_box);
-        auto current_volume = starting_box.volume().get_d();
-        interval_probabilities.insert(icell.id(),current_volume);
-        total_volume += current_volume;
-    }
+                SizeType ival_xtime = static_cast<SizeType>(sw.elapsed_seconds()*1000000);
 
-    double maximum_interval_probability = 0;
-    for (auto& p : interval_probabilities) {
-        maximum_interval_probability = std::max(maximum_interval_probability,p.second);
-    }
+                Vector<Map<SizeType,double>> point_probabilities(point_accuracy+1,Map<SizeType,double>());
 
-    Set<SizeType> to_remove;
-    double remaining_total_probability = 0;
-    for (auto& p : interval_probabilities) {
-        p.second = p.second/total_volume;
-        if (p.second < probability_threshold*maximum_interval_probability) to_remove.insert(p.first);
-        else remaining_total_probability += p.second;
-    }
+                Vector<SizeType> pt_xtime(point_accuracy+1);
 
-    interval_probabilities.remove_keys(to_remove);
-    CONCLOG_PRINTLN("Interval probabilities:")
-    for (auto& p : interval_probabilities) {
-        p.second = p.second/remaining_total_probability;
-        CONCLOG_PRINTLN(p.first << ": " << p.second)
-    }
+                for (SizeType acc=0; acc<=point_accuracy;++acc) {
+                    CONCLOG_PRINTLN_VAR_AT(1,acc)
+                    sw.restart();
+                    GridTreePaving sampling(state_grid);
+                    sampling.adjoin_outer_approximation(shrink(state_cell.box(),eps),acc);
 
-    sw.click();
-    CONCLOG_PRINTLN("Done in " << sw.elapsed_seconds() << " seconds.")
-    }
+                    GridTreePaving intersected_sampling = intersection(state_paving,sampling);
+                    intersected_sampling.mince(acc);
 
-    Vector<Map<SizeType,double>> point_probabilities(point_accuracy,Map<SizeType,double>());
+                    Map<SizeType,SizeType> occurrencies;
+                    SizeType total_occurrencies = 0;
 
-    {
+                    for (auto const& c : sampling) {
+                        auto input_box = product(ExactBoxType(c.box().centre()), control_cell.box());
 
-    Real eps = 1e-6_dec;
-    UpperBoxType initial_ubx({{0.0_dec+eps,0.5_dec-eps},{0.0_dec+eps,0.5_dec-eps}, {0.0_dec+eps,pi/4_dec-eps}});
+                        auto final_box = shrink(cast_exact_box(apply(dynamics, input_box).bounding_box()),eps);
 
-    for (SizeType acc=1; acc<=point_accuracy;++acc) {
-        CONCLOG_PRINTLN_VAR(acc)
-        sw.restart();
-        GridTreePaving sampling(state_grid);
-        sampling.adjoin_outer_approximation(initial_ubx,acc);
-        CONCLOG_PRINTLN_VAR(sampling.size())
+                        GridTreePaving target(state_grid);
+                        target.adjoin_outer_approximation(final_box,0);
+                        target.restrict(state_paving);
+                        target.mince(0);
 
-        GridTreePaving intersected_sampling = intersection(domain_paving,sampling);
+                        for (auto const& tcell : target) {
+                            auto itcell = identified_cell_factory.create(tcell);
+                            if (occurrencies.has_key(itcell.id()))
+                                occurrencies[itcell.id()]++;
+                            else
+                                occurrencies.insert(itcell.id(),1);
+                            ++total_occurrencies;
+                        }
+                    }
 
-        Map<SizeType,SizeType> occurrencies;
-        SizeType total_occurrencies = 0;
+                    for (auto const& occ : occurrencies) {
+                        point_probabilities.at(acc).insert(occ.first,static_cast<double>(occ.second)/total_occurrencies);
+                    }
 
-        ProgressIndicator indicator(sampling.size());
-        for (auto const& c : sampling) {
-            auto starting_pt = c.box().centre();
+                    sw.click();
+                    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.")
+                    pt_xtime.at(acc) = static_cast<SizeType>(sw.elapsed_seconds()*1000000);
 
-            RealBox real_starting_pt(full_spc.dimension());
-            for (SizeType i=0; i<state_spc.size();++i) {
-                real_starting_pt[i] = {cast_exact(starting_pt[i].get_d()),cast_exact(starting_pt[i].get_d())};
-            }
-            for (SizeType i=state_spc.size(); i<full_spc.size();++i) {
-                real_starting_pt[i] = {cast_exact(0.0),cast_exact(3.141592/4)};
-            }
-            RealVariablesBox starting_variables_pt(full_spc,real_starting_pt);
-
-            CONCLOG_RUN_AT(1,auto orbit = evolver.orbit(starting_variables_pt,evolution_time,Semantics::UPPER))
-            auto final_bx = orbit.final().bounding_box();
-
-            UpperBoxType final_projected_bx(3,UpperIntervalType::empty_interval());
-            {
-                SizeType idx = 0;
-                for (auto const& v : final_bx.space().variables()) {
-                    if (state_spc.contains(v)) {
-                        final_projected_bx[idx] = final_bx[v];
-                        ++idx;
+                    for (auto const& pp : point_probabilities.at(acc)) {
+                        CONCLOG_PRINTLN_AT(1,pp.first << ": " << pp.second)
                     }
                 }
+
+                double interval_deviation = 0;
+                {
+                    for (auto const& p : interval_probabilities) {
+                        auto val = point_probabilities.at(point_accuracy).find(p.first);
+                        if (val == point_probabilities.at(point_accuracy).end())
+                            interval_deviation += p.second*p.second;
+                        else
+                            interval_deviation += pow(p.second-val->second,2);
+                    }
+                    interval_deviation = sqrt(interval_deviation/point_probabilities.at(point_accuracy).size());
+                    CONCLOG_PRINTLN_AT(1,"Interval deviation: " << interval_deviation)
+                }
+
+                SizeType reference_accuracy = 0;
+
+                Vector<double> point_deviations(point_accuracy,0.0);
+                bool found_reference = false;
+                for (SizeType acc=0;acc<point_accuracy;++acc) {
+                    CONCLOG_PRINTLN_AT(1,"Accuracy " << acc)
+                    for (auto const& p : point_probabilities.at(point_accuracy)) {
+                        auto val = point_probabilities.at(acc).find(p.first);
+                        if (val == point_probabilities.at(acc).end())
+                            point_deviations.at(acc) += p.second*p.second;
+                        else
+                            point_deviations.at(acc) += pow(p.second-val->second,2);
+                    }
+                    point_deviations.at(acc) = sqrt(point_deviations.at(acc)/point_probabilities.at(point_accuracy).size());
+                    CONCLOG_PRINTLN_AT(1,"Deviation: " << point_deviations.at(acc))
+                    if ((not found_reference) and acc > 0 and point_deviations.at(acc) < interval_deviation) {
+                        found_reference = true;
+                        reference_accuracy = acc-1;
+                    }
+                }
+
+                auto xratio = static_cast<double>(pt_xtime.at(reference_accuracy))/static_cast<double>(ival_xtime);
+                CONCLOG_PRINTLN("Execution time ratio: " << xratio)
+            } else {
+                CONCLOG_PRINTLN("Targets are outside the domain, skipping")
             }
 
-            GridTreePaving target(state_grid);
-            target.adjoin_outer_approximation(final_projected_bx,0);
-
-            for (auto const& tcell : target) {
-                auto itcell = identified_cell_factory.create(tcell);
-                if (occurrencies.has_key(itcell.id()))
-                    occurrencies[itcell.id()]++;
-                else
-                    occurrencies.insert(itcell.id(),1);
-                ++total_occurrencies;
-            }
             indicator.update_current(indicator.current_value()+1);
             CONCLOG_SCOPE_PRINTHOLD("[" << indicator.symbol() << "] " << indicator.percentage() << "% ")
         }
-
-        for (auto const& occ : occurrencies) {
-            point_probabilities.at(acc-1).insert(occ.first,static_cast<double>(occ.second)/total_occurrencies);
-        }
-
-        sw.click();
-        CONCLOG_PRINTLN("Done in " << sw.elapsed_seconds() << " seconds.")
-
-        for (auto const& pp : point_probabilities.at(acc-1)) {
-            CONCLOG_PRINTLN(pp.first << ": " << pp.second)
-        }
     }
-    }
-
-    {
-        double total_deviation = 0;
-        for (auto const& p : interval_probabilities) {
-            auto val = point_probabilities.at(point_accuracy-1).find(p.first);
-            if (val == point_probabilities.at(point_accuracy-1).end())
-                total_deviation += p.second*p.second;
-            else
-                total_deviation += pow(p.second-val->second,2);
-        }
-        total_deviation = sqrt(total_deviation/point_probabilities.at(point_accuracy-1).size());
-        CONCLOG_PRINTLN("Interval deviation: " << total_deviation)
-    }
-
-    for (SizeType acc=0;acc<point_accuracy-1;++acc) {
-        CONCLOG_PRINTLN("Accuracy " << acc+1)
-        double total_deviation = 0;
-        for (auto const& p : point_probabilities.at(point_accuracy-1)) {
-            auto val = point_probabilities.at(acc).find(p.first);
-            if (val == point_probabilities.at(acc).end())
-                total_deviation += p.second*p.second;
-            else
-                total_deviation += pow(p.second-val->second,2);
-        }
-        total_deviation = sqrt(total_deviation/point_probabilities.at(point_accuracy-1).size());
-        CONCLOG_PRINTLN("Deviation: " << total_deviation)
-    }
-
-
-
-    /*
-    RealBox real_starting_box(full_spc.dimension());
-    for (SizeType i=0; i<state_spc.size();++i) {
-        real_starting_box[i] = {cast_exact(starting_box[i].lower_bound().get_d()),cast_exact(starting_box[i].upper_bound().get_d())};
-    }
-    for (SizeType i=state_spc.size(); i<full_spc.size();++i) {
-        real_starting_box[i] = {cast_exact(final_box.continuous_set()[i].lower_bound().get_d()),cast_exact(final_box.continuous_set()[i].upper_bound().get_d())};
-    }
-    RealVariablesBox initial(full_spc,real_starting_box);
-    */
 }

c6d51dbef5f33eb75475cd65241c94521ac7c087
Luca Geretti <luca.geretti@gmail.com>
2023-11-20 16:48:53 -0800

    Add ability to specify dynamics in differential form + the time step

--- experimental/examples/discrete/probability_map.cpp
+++ experimental/examples/discrete/probability_map.cpp
@@ -28,64 +28,67 @@
 #include "reach_avoid.hpp"
 using namespace Ariadne;
 
+Grid make_grid_from_lengths(RealSpace const& spc, List<Pair<RealVariable,Real>> const& lengths) {
+    Vector<Real> result(spc.dimension());
+    {
+        SizeType idx = 0;
+        for (auto const& v : spc.variables()) {
+            for (auto const& gl : lengths) {
+                if (gl.first == v) {
+                    result[idx] = gl.second;
+                    ++idx;
+                    break;
+                }
+            }
+        }
+    }
+    return Grid(result);
+}
+
+GridTreePaving make_paving(Grid const& grid, UpperBoxType const& domain) {
+    GridTreePaving result(grid);
+    result.adjoin_outer_approximation(domain,0);
+    result.mince(0);
+
+    return result;
+}
+
 void ariadne_main()
 {
-    Real deltat=0.1_dec, v=3;
-    RealVariable x("x"), y("y"), theta("theta"), u("u");
-    IteratedMap sys({next(x)=x+deltat*v*cos(theta),next(y)=y+deltat*v*sin(theta),next(theta)= theta+u,next(u)=u});
+    //! Setup
 
-    FloatDP eps(1e-8_x,DoublePrecision());
-    double probability_threshold = 0.05;
-    SizeType point_accuracy = 6;
+    RealConstant v("v",3);
+    RealVariable x("x"), y("y"), theta("theta"), u("u");
 
     RealSpace state_spc({x,y,theta});
     RealSpace control_spc({u});
 
+    Real deltat = 0.1_dec;
+
+    List<DottedRealAssignment> differential_dynamics = {{dot(x)=v*cos(theta),dot(y)=v*sin(theta),dot(theta)=u/deltat}};
+
+    FloatDP eps(1e-8_x,DoublePrecision());
+    double probability_threshold = 0.05;
+    SizeType point_accuracy = 7;
+
     List<Pair<RealVariable,Real>> state_grid_lengths({{x,0.5_dec},{y,0.5_dec},{theta,pi/4}});
     List<Pair<RealVariable,Real>> control_grid_lengths({{u,pi/4}});
 
-    Vector<Real> real_state_grid_lengths(state_spc.dimension());
-    {
-        SizeType idx = 0;
-        for (auto const& v : state_spc.variables()) {
-            for (auto const& gl : state_grid_lengths) {
-                if (gl.first == v) {
-                    real_state_grid_lengths[idx] = gl.second;
-                    ++idx;
-                    break;
-                }
-            }
-        }
-    }
-    Grid state_grid(real_state_grid_lengths);
-
-    Vector<Real> real_control_grid_lengths(control_spc.dimension());
-    {
-        SizeType idx = 0;
-        for (auto const& v : control_spc.variables()) {
-            for (auto const& gl : control_grid_lengths) {
-                if (gl.first == v) {
-                    real_control_grid_lengths[idx] = gl.second;
-                    ++idx;
-                    break;
-                }
-            }
-        }
-    }
-    Grid control_grid(real_control_grid_lengths);
-
-    GridTreePaving state_paving(state_grid);
     UpperBoxType state_domain({{0.0_dec+eps,5_dec-eps},{0.0_dec+eps,5_dec-eps}, {0.0_dec+eps,2*pi-eps}});
-    state_paving.adjoin_outer_approximation(state_domain,0);
-    state_paving.mince(0);
-
-    GridTreePaving control_paving(control_grid);
     UpperBoxType control_domain({{0.0_dec+eps,2*pi-eps}});
-    control_paving.adjoin_outer_approximation(control_domain,0);
-    control_paving.mince(0);
+
+    //! Processing
+
+    RealSpace full_spc = state_spc;
+    full_spc.adjoin(control_spc);
+
+    Grid state_grid = make_grid_from_lengths(state_spc,state_grid_lengths);
+    Grid control_grid = make_grid_from_lengths(control_spc,control_grid_lengths);
+
+    GridTreePaving state_paving = make_paving(state_grid,state_domain);
+    GridTreePaving control_paving = make_paving(control_grid,control_domain);
 
     IdentifiedCellFactory::HashTableType state_cells_ids;
-
     auto default_cell_extent = state_paving.begin()->root_extent();
     CONCLOG_PRINTLN_VAR(default_cell_extent)
     CONCLOG_PRINTLN_VAR(state_paving.size())
@@ -102,11 +105,22 @@ void ariadne_main()
         for (auto const& control_cell : control_paving)
             state_control_boxes.push_back(make_pair(state_cell.box(), control_cell.box()));
 
-    BetterThreads::StaticWorkload<Pair<ExactBoxType,ExactBoxType>> workload([&](Pair<ExactBoxType,ExactBoxType> const& sc_boxes){
+    std::atomic<double> sum_xratio = 0;
+    std::atomic<double> sum_effective_accuracy = 0;
+    std::atomic<SizeType> num_processed = 0;
 
-        auto dynamics = sys.function().zeros(state_spc.size(),sys.dimension());
-        for (SizeType i=0; i<state_spc.size(); ++i)
-            dynamics[i] = sys.function().get(i);
+
+    auto forward_dynamics = EffectiveVectorMultivariateFunction::zeros(state_spc.size(),full_spc.size());
+    auto backward_dynamics = EffectiveVectorMultivariateFunction::zeros(state_spc.size(),full_spc.size());
+    for(SizeType i=0; i!=state_spc.size(); ++i) {
+        forward_dynamics[i] = EffectiveScalarMultivariateFunction::coordinate(full_spc.size(),i) + deltat * make_function(full_spc,differential_dynamics.at(i).expression());
+        backward_dynamics[i] = EffectiveScalarMultivariateFunction::coordinate(full_spc.size(),i) - deltat * make_function(full_spc,differential_dynamics.at(i).expression());
+    }
+
+    CONCLOG_PRINTLN_VAR(forward_dynamics)
+    CONCLOG_PRINTLN_VAR(backward_dynamics)
+
+    BetterThreads::StaticWorkload<Pair<ExactBoxType,ExactBoxType>> workload([&](Pair<ExactBoxType,ExactBoxType> const& sc_boxes){
 
         Stopwatch<Microseconds> sw;
 
@@ -114,7 +128,7 @@ void ariadne_main()
 
         CONCLOG_PRINTLN_AT(1,"Computing using Interval Arithmetic...")
 
-        auto final_box = shrink(cast_exact_box(apply(dynamics, product(sc_boxes.first, sc_boxes.second)).bounding_box()),eps);
+        auto final_box = shrink(cast_exact_box(apply(forward_dynamics, product(sc_boxes.first, sc_boxes.second)).bounding_box()),eps);
 
         GridTreePaving final_paving(state_grid);
         final_paving.adjoin_outer_approximation(final_box,0);
@@ -178,7 +192,7 @@ void ariadne_main()
                 for (auto const& c : sampling) {
                     auto input_box = product(ExactBoxType(c.box().centre()), sc_boxes.second);
 
-                    auto final_box = shrink(cast_exact_box(apply(dynamics, input_box).bounding_box()),eps);
+                    auto final_box = shrink(cast_exact_box(apply(forward_dynamics, input_box).bounding_box()),eps);
 
                     GridTreePaving target(state_grid);
                     target.adjoin_outer_approximation(final_box,0);
@@ -242,12 +256,32 @@ void ariadne_main()
                 }
             }
 
-            auto xratio = static_cast<double>(pt_xtime.at(reference_accuracy))/static_cast<double>(ival_xtime);
-            CONCLOG_PRINTLN("Execution time ratio: " << xratio)
+            auto effective_pt_xtime = static_cast<double>(pt_xtime.at(reference_accuracy));
+            auto effective_accuracy = static_cast<double>(reference_accuracy);
+            if (point_deviations.at(reference_accuracy) > interval_deviation) {
+                auto log_ref_pt_dev = log(point_deviations.at(reference_accuracy));
+                auto log_next_pt_dev = log(point_deviations.at(reference_accuracy+1));
+                auto log_ivl_dev = log(interval_deviation);
+                auto fraction = (log_ref_pt_dev-log_ivl_dev)/(log_ref_pt_dev-log_next_pt_dev);
+                effective_pt_xtime = static_cast<double>(pt_xtime.at(reference_accuracy))+fraction*(static_cast<double>(pt_xtime.at(reference_accuracy+1)-pt_xtime.at(reference_accuracy)));
+                effective_accuracy = static_cast<double>(reference_accuracy)+fraction;
+            }
+
+            auto xratio = effective_pt_xtime/static_cast<double>(ival_xtime);
+
+            if (interval_deviation != 0) {
+                CONCLOG_PRINTLN_AT(1,"Execution time ratio: " << xratio)
+                ++num_processed;
+                sum_xratio = sum_xratio + xratio;
+                sum_effective_accuracy = sum_effective_accuracy + effective_accuracy;
+                CONCLOG_SCOPE_PRINTHOLD("avg ratio: " << sum_xratio/num_processed << ", avg effective accuracy: " << sum_effective_accuracy/num_processed)
+            }
         } else {
-            CONCLOG_PRINTLN("Targets are outside the domain, skipping")
+            CONCLOG_PRINTLN_AT(1,"Targets are outside the domain, skipping")
         }
     });
     
     workload.append(state_control_boxes).process();
+
+    CONCLOG_PRINTLN("Avg ratio: " << sum_xratio/num_processed << ", avg effective accuracy: " << sum_effective_accuracy/num_processed)
 }

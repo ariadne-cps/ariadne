8adedc0c332ee5d45afaef73c4afda0541549d1c
Luca Geretti <luca.geretti@icloud.com>
2026-09-26 16:38:55 +0200

    Restore original Van der Pol workflow with preconditioned integrator

--- examples/continuous/vanderpol.cpp
+++ examples/continuous/vanderpol.cpp
@@ -5,14 +5,25 @@
  *
  ****************************************************************************/
 
-#include "utility/stopwatch.hpp"
-#include "function/taylor_function.hpp"
-#include "function/constraint.hpp"
-#include "dynamics/enclosure.hpp"
-#include "ariadne_main.hpp"
+/*
+ *  This file is part of Ariadne.
+ *
+ *  Ariadne is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Ariadne is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Ariadne.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-#include <array>
-#include <memory>
+#include "utility/stopwatch.hpp"
+#include "ariadne_main.hpp"
 
 void ariadne_main()
 {
@@ -20,56 +31,52 @@ void ariadne_main()
 
     RealConstant mu("mu",1);
     RealVariable x("x"), y("y");
-    VectorField dynamics({dot(x)=y, dot(y)=mu*y*(1-sqr(x))-x});
 
-    ThresholdSweeper<FloatDP> sweeper(DoublePrecision(),1e-12);
+    VectorField dynamics({dot(x)=y, dot(y)= mu*y*(1-sqr(x))-x});
 
-    Real x0=1.40_dec;
-    Real y0=2.30_dec;
-    Real eps_x0=0.15_dec;
-    Real eps_y0=0.05_dec;
-    RealExpressionBoundedConstraintSet initial_set({
-        x0-eps_x0<=x<=x0+eps_x0,
-        y0-eps_y0<=y<=y0+eps_y0
-    });
+    VectorFieldSimulator simulator(dynamics);
+    simulator.configuration().set_step_size(0.02);
 
-    auto configure_evolver = [](VectorFieldEvolver& evolver, ExactDouble max_step) {
-        evolver.configuration().set_maximum_enclosure_radius(1.0);
-        evolver.configuration().set_maximum_step_size(max_step);
-        evolver.configuration().set_maximum_spacial_error(1e-6);
-        evolver.configuration().set_enable_reconditioning(false);
-    };
+    StepMaximumError max_err=1e-6;
 
-    // Snapshot the actually carried Taylor expansion rather than cumulative
-    // sweeper events. The evolver diagnostic reports the input and output
-    // normalised mappings at steps 500, 1000, 1500 and 2000.
-    const ExactDouble loose_tolerance=1e-2_x;
-    const ExactDouble plateau_step=0.0025_x;
-
-    ThresholdSweeper<FloatDP> probe_sweeper(DoublePrecision(),3e-14);
-    PreconditionedGradedTaylorSeriesIntegrator integrator(
-        StepMaximumError(loose_tolerance),probe_sweeper,
-        lipschitz_tolerance=0.5_x,
-        minimum_spacial_order=5,minimum_temporal_order=5,
-        maximum_spacial_order=5,maximum_temporal_order=5);
+    PreconditionedGradedTaylorSeriesIntegrator integrator(max_err);
     integrator.set_preconditioning(TaylorSeriesPreconditioning::QR);
-    integrator.set_diagnostics(false);
-    integrator.set_carried_expansion_diagnostics(false);
 
     VectorFieldEvolver evolver(dynamics,integrator);
-    configure_evolver(evolver,plateau_step);
+    evolver.configuration().set_maximum_enclosure_radius(1.0);
+    evolver.configuration().set_maximum_step_size(0.02);
+    evolver.configuration().set_maximum_spacial_error(1e-6);
+    CONCLOG_PRINTLN(evolver.configuration());
 
-    set_taylor_model_product_profile_enabled(false);
-    set_taylor_model_early_discard_enabled(false);
-    set_taylor_model_incremental_sweep_enabled(false);
-    set_taylor_model_product_accumulator_enabled(true);
-    set_taylor_model_dense_accumulator_enabled(true);
+    Real x0 = 1.40_dec;
+    Real y0 = 2.40_dec;
+    Real eps_x0 = 0.15_dec;
+    Real eps_y0 = 0.05_dec;
 
-    auto orbit=evolver.orbit(
-        initial_set,Real(7.00_dec),Semantics::UPPER);
+    RealExpressionBoundedConstraintSet initial_set({x0-eps_x0<=x<=x0+eps_x0,y0-eps_y0<=y<=y0+eps_y0});
 
-    set_taylor_model_dense_accumulator_enabled(false);
-    set_taylor_model_product_accumulator_enabled(false);
-    set_taylor_model_incremental_sweep_enabled(true);
+    CONCLOG_PRINTLN("Initial set: " << initial_set);
+    Real evolution_time = 7;
 
+    Stopwatch<Milliseconds> sw;
+    CONCLOG_PRINTLN("Computing simulation...");
+    auto simulation = simulator.orbit(initial_set,evolution_time);
+    sw.click();
+    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.");
+
+    CONCLOG_PRINTLN("Plotting...");;
+    LabelledFigure fig=LabelledFigure({-2.5<=x<=2.5,-3<=y<=3});
+    fig.draw(simulation);
+    fig.write("vanderpol_simulation");
+
+    sw.restart();
+    CONCLOG_PRINTLN("Computing evolution... ");
+    auto evolution = evolver.orbit(initial_set,evolution_time,Semantics::UPPER);
+    sw.click();
+    CONCLOG_PRINTLN_AT(1,"Done in " << sw.elapsed_seconds() << " seconds.");
+
+    CONCLOG_PRINTLN("Plotting...");
+    fig.clear();
+    fig.draw(evolution);
+    fig.write("vanderpol_evolution");
 }
